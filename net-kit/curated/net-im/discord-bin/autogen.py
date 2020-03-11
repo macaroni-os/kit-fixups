78a56cd8123d502a95fe4da0846982ca5daef4fb
Daniel Robbins <drobbins@funtoo.org>
2020-03-10 21:22:07 -0600

    New autogen stuff.

---
+++ net-kit/curated/net-im/discord-bin/autogen.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+async def generate(hub):
+
+	url = await hub.pkgtools.fetch.get_url_from_redirect("https://discordapp.com/api/download?platform=linux&format=deb")
+
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		hub,
+		name="discord-bin",
+		cat="net-im",
+		version=url.split("/")[-1].lstrip("discord-").rstrip(".deb"),
+		artifacts=[hub.pkgtools.ebuild.Artifact(url=url)]
+	)
+
+	ebuild.push()
+
+# vim: ts=4 sw=4 noet

79e63be7326954b154f968bcec811d9141f35e2a
Daniel Robbins <drobbins@funtoo.org>
2020-03-18 20:58:12 -0600

    FL-7042: add firefox-bin with autogen.

---
+++ browser-kit/curated/www-client/firefox-bin/autogen.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import json
+
+def get_artifact(hub, version, arch):
+	if arch == "amd64":
+		moz_arch = "x86_64"
+	elif arch == "x86":
+		moz_arch = "i686"
+	url = f"https://archive.mozilla.org/pub/firefox/releases/{version}/linux-{moz_arch}/en-US/firefox-{version}.tar.bz2"
+	final_name = f'firefox-bin_{moz_arch}-{version}.tar.bz2'
+	return hub.pkgtools.ebuild.Artifact(
+		url=url,
+		final_name=final_name
+	)
+
+async def generate(hub):
+
+	json_data = await hub.pkgtools.fetch.get_page("https://product-details.mozilla.org/1.0/firefox_versions.json")
+	json_dict = json.loads(json_data)
+	version = json_dict["LATEST_FIREFOX_VERSION"]
+
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		hub,
+		name="firefox-bin",
+		cat="www-client",
+		version=json_dict["LATEST_FIREFOX_VERSION"],
+		artifacts=[
+			get_artifact(hub, version, "amd64"),
+			get_artifact(hub, version, "x86")
+		]
+	)
+	ebuild.push()
+
+# vim: ts=4 sw=4 noet

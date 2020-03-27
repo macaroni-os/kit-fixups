93ddfaf87265c226908415dea984b048a941393e
Daniel Robbins <drobbins@funtoo.org>
2020-03-27 16:38:15 -0600

    Adding anydesk-bin (good remote desktop client) with autogen.

---
+++ desktop-kit/curated/net-misc/anydesk-bin/autogen.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import re
+
+async def generate(hub, **pkginfo):
+
+	html = await hub.pkgtools.fetch.get_page("https://anydesk.com/en/downloads/linux")
+	match = re.search(r"https://download.anydesk.com/linux/anydesk-(.\..\..)-amd64.tar.gz", html)
+	url = match.group(0) # the entire match
+	version = match.group(1) # just the version
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		hub,
+		**pkginfo,
+		version=version,
+		artifacts=[hub.pkgtools.ebuild.Artifact(hub, url=url)]
+	)
+
+	ebuild.push()
+
+# vim: ts=4 sw=4 noet

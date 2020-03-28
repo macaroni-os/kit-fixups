ac71a5d216907d454de9cd05c09b2c18d04d8db8
Daniel Robbins <drobbins@funtoo.org>
2020-03-27 21:42:40 -0600

    FL-7055: add dynamic langpack support to firefox-bin.

--- browser-kit/curated/www-client/firefox-bin/autogen.py
+++ browser-kit/curated/www-client/firefox-bin/autogen.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
 import json
+import re
+
+async def get_lang_artifacts(hub, version):
+	lang_page = await hub.pkgtools.fetch.get_page(f"https://archive.mozilla.org/pub/firefox/releases/{version}/linux-x86_64/xpi/")
+	lang_codes = []
+	artifacts = []
+	for lang_path in re.findall(f'/pub/firefox/releases/74.0/linux-x86_64/xpi/[^"]*\.xpi', lang_page):
+		lang_code = lang_path.split('/')[-1].split('.')[0]
+		lang_codes.append(lang_code)
+		artifacts.append(hub.pkgtools.ebuild.Artifact(hub, url='https://archive.mozilla.org' + lang_path, final_name=f'firefox-{version}-{lang_code}.xpi'))
+	return dict(
+		artifacts=artifacts,
+		lang_codes=lang_codes
+	)
 
 def get_artifact(hub, version, arch):
 	if arch == "amd64":
@@ -16,17 +30,19 @@ def get_artifact(hub, version, arch):
 	)
 
 async def generate(hub, **pkginfo):
-
 	json_data = await hub.pkgtools.fetch.get_page("https://product-details.mozilla.org/1.0/firefox_versions.json")
 	json_dict = json.loads(json_data)
 	version = json_dict["LATEST_FIREFOX_VERSION"]
-
+	lang_data = await get_lang_artifacts(hub, version)
+	print(lang_data)
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(hub,
 		**pkginfo,
 		version=version,
+		lang_codes=' '.join(sorted(lang_data['lang_codes'])),
 		artifacts=[
 			get_artifact(hub, version, "amd64"),
-			get_artifact(hub, version, "x86")
+			get_artifact(hub, version, "x86"),
+			*lang_data['artifacts']
 		]
 	)
 	ebuild.push()

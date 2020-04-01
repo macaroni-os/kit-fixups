ac7e3da0279783e1750fa94d31e2fde16b897423
Daniel Robbins <drobbins@funtoo.org>
2020-03-31 20:14:52 -0600

    Brave-bin's JSON sometimes doesn't list any non-prelease releases in the Release channel. So I modified the code to try the Release channel first but fall back to Beta, etc. if no releases are found.
    I also added a helpful message about getting brave-bin to run
    when using Funtoo's default debian-sources(-lts) kernels.

--- browser-kit/curated/www-client/brave-bin/autogen.py
+++ browser-kit/curated/www-client/brave-bin/autogen.py
@@ -1,23 +1,38 @@
 #!/usr/bin/env python3
 
 import json
-from re import match
 
-def get_latest_stable_version_for_desktop(releases):
-	stable_desktop_releases = list(filter(lambda x: match(r"^Release Channel v[0-9.]+$", x), releases))
-	stable_desktop_releases.sort()
-	return stable_desktop_releases.pop().split('v').pop()
+def find_release(json_dict, channel='Release'):
+	releases = filter(lambda x: x['name'].startswith(f"{channel} Channel") and x['prerelease'] is False and not x['name'].endswith('-android'), json_dict)
+	releases = list(releases)
+	if not len(releases):
+		return None
+	return sorted(releases, key=lambda x: x['tag_name'])[-1]
 
 async def generate(hub, **pkginfo):
 
 	json_data = await hub.pkgtools.fetch.get_page("https://api.github.com/repos/brave/brave-browser/releases")
 	json_dict = json.loads(json_data)
-	version = get_latest_stable_version_for_desktop(list(map(lambda x: x['name'], list(filter(lambda x: x['prerelease'] == False, json_dict)))))
+
+	# Try to use the latest release version, but fall back to latest nightly if none found:
+	release = None
+	for channel in [ 'Release', 'Beta', 'Nightly' ]:
+		release = find_release(json_dict, channel=channel)
+		if release:
+			break
+
+	if release is None:
+		raise hub.pkgtools.ebuild.BreezyError("Can't find a suitable release of Brave.")
+
+	version = release['tag_name'][1:] # strip leading 'v'
+
+	url = list(filter(lambda x: x['browser_download_url'].endswith('-linux-x64.zip'), release['assets']))[0]['browser_download_url']
+
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
 		version=version,
 		artifacts=[
-			hub.pkgtools.ebuild.Artifact(url=f'https://github.com/brave/brave-browser/releases/download/v{version}/brave-v{version}-linux-x64.zip')
+			hub.pkgtools.ebuild.Artifact(url=url)
 		]
 	)
 	ebuild.push()

49b36b6c148885fdfd35a33562b91defd589e87d
Daniel Robbins <drobbins@funtoo.org>
2020-04-24 19:09:19 -0600

    Fixing passwordsafe autogen which was doing a bit too much when there is a more robust way.

--- desktop-kit/curated/app-admin/passwordsafe/autogen.py
+++ desktop-kit/curated/app-admin/passwordsafe/autogen.py
@@ -3,15 +3,10 @@
 import json
 from re import match
 
-def get_latest_linux_version(releases):
-	linux_releases = list(filter(lambda x: match(r"^PasswordSafe Linux release .+", x), releases))
-	linux_releases.sort()
-	return linux_releases.pop().split("release ").pop()
-
 async def generate(hub, **pkginfo):
 	json_data = await hub.pkgtools.fetch.get_page("https://api.github.com/repos/pwsafe/pwsafe/releases")
-	json_dict = json.loads(json_data)
-	version = get_latest_linux_version(list(map(lambda x: x['name'], list(filter(lambda x: x['prerelease'] == False, json_dict)))))
+	json_list = json.loads(json_data)
+	version = json_list[0]["tag_name"]
 	url = f'https://github.com/pwsafe/pwsafe/archive/{version}.tar.gz'
 	final_name = f'{pkginfo["name"]}-{version}.tar.gz'
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(

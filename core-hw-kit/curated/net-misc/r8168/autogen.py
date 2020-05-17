7d5717cfc1076a3ee1d9bd95d04df47883e332d8
drobbins <drobbins@funtoo.org>
2020-05-17 15:43:26 +0000

    Merge pull request #577 in CORE/kit-fixups from ~D4G33Z/kit-fixups-r8168:r8168-unrestricted to master
    Squashed commit of the following:
    
    commit 917fadbf49a6c17c2f5ae3a620e4c04e4c3f5f3a
    Author: jeff <jeff@azeus>
    Date:   Sat May 16 14:20:04 2020 -0400
    
        use github api
    
    commit 9b17053f988e07546812939f753500cced3bdcae
    Author: jeff <jeff@azeus>
    Date:   Sat May 16 02:38:47 2020 -0400
    
        initial commit

---
+++ core-hw-kit/curated/net-misc/r8168/autogen.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import json
+
+GITHUB_REPO='r8168'
+GITHUB_USER='mtorromeo'
+
+# mimic the Portage package name variable for clarity in the tarball renaming
+PN=GITHUB_REPO
+
+async def generate(hub, **pkginfo):
+	json_data = await hub.pkgtools.fetch.get_page(f"https://api.github.com/repos/{GITHUB_USER}/{GITHUB_REPO}/tags")
+	tags = json.loads(json_data)
+	#the latest tag is the first dict in the returned list of tag dicts
+	version = tags[0].get('name')
+	tarball_url = tags[0].get('tarball_url')
+	template_args = dict(
+		GITHUB_USER=GITHUB_USER,
+	)
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		version=version,
+		artifacts=[
+			hub.pkgtools.ebuild.Artifact(
+				url=tarball_url,
+				final_name=f'{PN}-{version}.tgz'),
+		],
+		**template_args,
+	)
+	ebuild.push()
+
+# vim: ts=4 sw=4 noet

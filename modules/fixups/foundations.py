a1cda738db63385dbe027a104148046d6fc74fa8
Daniel Robbins <drobbins@funtoo.org>
2020-06-05 12:19:34 -0600

    Add functionality to NOT skip duplicate catpkgs when 'source' is None in foundations.py.

--- modules/fixups/foundations.py
+++ modules/fixups/foundations.py
@@ -84,7 +84,7 @@ class KitFoundation:
 				{'name': 'core-server-kit', 'branch': '1.4-release', 'source': 'funtoo_release_1.4', 'stability': KitStabilityRating.PRIME},
 				{'name': 'xorg-kit', 'branch': '1.20-release', 'stability': KitStabilityRating.PRIME, "type": KitType.INDEPENDENTLY_MAINTAINED},  # primary
 				{'name': 'core-gl-kit', 'branch': '1.4-release', 'source': 'funtoo_release_1.4', 'stability': KitStabilityRating.PRIME},
-				{'name': 'core-gl-kit', 'branch': '2.0-release', 'source': None, 'stability': KitStabilityRating.PRIME},
+				{'name': 'core-gl-kit', 'branch': '2.0-release', 'source': None, 'stability': KitStabilityRating.BETA},
 				{'name': 'security-kit', 'branch': '1.4-release', 'source': 'funtoo_release_1.4_bump', 'stability': KitStabilityRating.PRIME},
 				{'name': 'gnome-kit', 'branch': '3.36-prime', 'stability': KitStabilityRating.PRIME, "type": KitType.INDEPENDENTLY_MAINTAINED},  # primary
 				{'name': 'gnome-kit', 'branch': '3.34-prime', 'stability': KitStabilityRating.PRIME, "type": KitType.INDEPENDENTLY_MAINTAINED},

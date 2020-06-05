eb3f1e3e0444238fffd56cc79e9e9ac4262dd2ba
Daniel Robbins <drobbins@funtoo.org>
2020-06-05 16:52:22 -0600

    More work on this.

--- core-gl-kit/2.0-release/x11-base/xorg-drivers/xorg-drivers-1.20.ebuild
+++ core-gl-kit/2.0-release/x11-base/xorg-drivers/xorg-drivers-1.20.ebuild
@@ -100,7 +100,7 @@ PDEPEND="
 	video_cards_vesa?          ( x11-drivers/xf86-video-vesa )
 	video_cards_via?           ( x11-drivers/xf86-video-openchrome )
 	video_cards_virtualbox?    ( || (
-					x11-drivers/xf86-video-vboxvideo
+			x11-drivers/xf86-video-vbox
 					>=x11-drivers/xf86-video-virtualbox-5.1.14
 					) )
 	video_cards_vmware?        ( >=x11-drivers/xf86-video-vmware-13.3.0 )
--- core-gl-kit/2.0-release/x11-drivers/generator.py
+++ core-gl-kit/2.0-release/x11-drivers/generator.py
@@ -11,7 +11,7 @@ async def generate(hub, **pkginfo):
 	gitlab_id = pkginfo['gitlab_id']
 	json_data = await hub.pkgtools.fetch.get_page(f"https://gitlab.freedesktop.org/api/v4/projects/{gitlab_id}/repository/tags")
 	json_list = json.loads(json_data)
-	version = json_list[0]['name'].lstrip(f"{name}-")
+	version = json_list[0]['name'][len(name):].lstrip('-')
 	url = f'https://gitlab.freedesktop.org/xorg/driver/{name}/-/archive/{name}-{version}/{name}-{name}-{version}.tar.bz2'
 	final_name = f'{name}-{version}-gitlab.tar.bz2'
 

9b4f724958c53a40a30b05368cccb3c2ed9d6164
drobbins <drobbins@funtoo.org>
2020-07-29 18:26:23 +0000

    Merge pull request #661 in CORE/kit-fixups from ~ALEX2101/kit-fixups:bugfix/FL-7270-net-libs-libtorrent-rasterbar-fails-to-build to master
    Squashed commit of the following:
    
    commit 8ddb4905088316728bdf3d80cde101031d97a614
    Author: alex2101 <alex2101@gmail.com>
    Date:   Wed Jul 29 08:50:04 2020 -0300
    
        FL-7270: Fix net-libs/libtorrent-rasterbar download source.

--- net-kit/curated/net-libs/libtorrent-rasterbar/autogen.py
+++ net-kit/curated/net-libs/libtorrent-rasterbar/autogen.py
@@ -3,19 +3,20 @@
 import json
 
 async def generate(hub, **pkginfo):
-    user = 'arvidn'
-    repo = 'libtorrent'
-    app = 'libtorrent-rasterbar'
-    json_data = await hub.pkgtools.fetch.get_page(f'https://api.github.com/repos/{user}/{repo}/releases/latest')
-    json_list = json.loads(json_data)
-    tag = json_list['tag_name']
-    version = json_list['name'].split('-', 1)[1]
-    url = f'https://github.com/{user}/{repo}/archive/{tag}.tar.gz'
-    final_name = f'{app}-{version}.tar.gz'
+	user = 'arvidn'
+	repo = 'libtorrent'
+	app = 'libtorrent-rasterbar'
+	json_data = await hub.pkgtools.fetch.get_page(f'https://api.github.com/repos/{user}/{repo}/releases/latest')
+	json_list = json.loads(json_data)
+	tag = json_list['tag_name']
+	version = json_list['name'].split('-', 1)[1]
+	url = f'https://github.com/{user}/{repo}/releases/download/{tag}/{app}-{version}.tar.gz'
 
-    ebuild = hub.pkgtools.ebuild.BreezyBuild(
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
 		version=version,
-		artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)]
+		artifacts=[hub.pkgtools.ebuild.Artifact(url=url)]
 	)
-    ebuild.push()
+	ebuild.push()
+
+# vim: ts=4 sw=4 noet

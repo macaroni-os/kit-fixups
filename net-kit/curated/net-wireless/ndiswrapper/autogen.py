435a6b3193652c31cdf37a994c3ad158f485343c
drobbins <drobbins@funtoo.org>
2020-08-23 16:39:03 +0000

    Merge pull request #702 in CORE/kit-fixups from ~ALEX2101/kit-fixups:bugfix/FL-7137-failed-to-build-ndiswrapper-1.61 to master
    Squashed commit of the following:
    
    commit cb928517a6d66b6493927b8872238b52c1b1cb70
    Author: alex2101 <alex2101@gmail.com>
    Date:   Sat Aug 22 07:24:48 2020 -0300
    
        FL-7137: Include net-wireless/ndiswrapper to the autogen framework.

---
+++ net-kit/curated/net-wireless/ndiswrapper/autogen.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import json
+import re
+
+async def generate(hub, **pkginfo):
+	json_data = await hub.pkgtools.fetch.get_page('https://sourceforge.net/projects/ndiswrapper/best_release.json')
+	json_dict = json.loads(json_data)
+	release_dict = json_dict['release']
+	version = (re.search(f'ndiswrapper-([0-9.]*).tar.gz', release_dict['url'])).group(1)
+	url = f'http://downloads.sourceforge.net/project/ndiswrapper/stable/ndiswrapper-{version}.tar.gz'
+	artifacts = [hub.pkgtools.ebuild.Artifact(url=url)]
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		version=version,
+		artifacts=artifacts,
+	)
+	ebuild.push()
+# vim: ts=4 sw=4 noet

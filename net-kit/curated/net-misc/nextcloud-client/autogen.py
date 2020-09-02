e7258215cb9da0fa57519d4c59f7469010ddacf1
Daniel Robbins <drobbins@funtoo.org>
2020-09-01 22:38:19 -0600

    FL-7361: improving nextcloud-client to be more autogen-worthy. Also removing a libvpx mask.

--- net-kit/curated/net-misc/nextcloud-client/autogen.py
+++ net-kit/curated/net-misc/nextcloud-client/autogen.py
@@ -5,17 +5,18 @@ import json
 async def generate(hub, **pkginfo):
 	github_user = 'nextcloud'
 	github_repo = 'desktop'
-	json_data = await hub.pkgtools.fetch.get_page(f'https://api.github.com/repos/{github_user}/{github_repo}/tags')
-	json_dict = json.loads(json_data)
-	for tag in json_dict:
-		if tag['name'].find("-beta") == -1:
-			version = tag['name']
-			break
+	json_list = await hub.pkgtools.fetch.get_page(f'https://api.github.com/repos/{github_user}/{github_repo}/releases', is_json=True)
+	for release in json_list:
+		if release['prerelease'] or release['draft']:
+			continue
+		version = release['tag_name'][1:]
+		url = release['tarball_url']
+		break
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
-		version=version[1:], # [1:] slices out the 'v' char in version
+		version=version,
 		artifacts=[
-			hub.pkgtools.ebuild.Artifact(url=f'https://github.com/{github_user}/{github_repo}/archive/{version}.tar.gz', final_name=f'{github_user}-{version[1:]}.tar.gz')
+			hub.pkgtools.ebuild.Artifact(url=url, final_name=f'nextcloud-desktop-{version}.tar.gz')
 		]
 	)
 	ebuild.push()

910f235cb64d9aa6180951b9008bf45a90187158
drobbins <drobbins@funtoo.org>
2020-09-09 22:27:17 +0000

    Merge pull request #719 in CORE/kit-fixups from ~SENYAI/kit-fixups-senyai:vscode-bin-autogen-r1 to master
    Squashed commit of the following:
    
    commit 8616a93b40490b6438c7d4b8cacde8067b3126db
    Author: Arseniy Terekhin <senyai@gmail.com>
    Date:   Thu Sep 10 00:49:59 2020 +0300
    
        Renamed variable name in vscode-bin/autogen.py
    
    commit 50d61aef4214f9c2d63e03cc8b46aaee4ba65fcc
    Author: Arseniy Terekhin <senyai@gmail.com>
    Date:   Wed Sep 9 23:39:53 2020 +0300
    
        vscode-bin missing permissions

--- editors-kit/curated/app-editors/vscode-bin/autogen.py
+++ editors-kit/curated/app-editors/vscode-bin/autogen.py
@@ -9,17 +9,22 @@ async def generate(hub, **pkginfo):
 	if len(versions) != 1:
 		raise hub.pkgtools.ebuild.BreezyError("vscode page was expected to have exactly one linux download link. The script needs updating")
 	url, version = versions[0]
-	source_artifact = hub.pkgtools.ebuild.Artifact(url=url, final_name=f'vscode-bin-{version}.tar.gz')
-	unpack_path = Path(source_artifact.extract_path)
-	src_path = next(unpack_path.iterdir())
-	sh_paths = [so_path.relative_to(src_path) for so_path in src_path.rglob('*.sh')]
-	so_paths = [so_path.relative_to(src_path) for so_path in src_path.rglob('*.so')]
+	artifact = hub.pkgtools.ebuild.Artifact(url=url, final_name=f'vscode-bin-{version}.tar.gz')
+	unpack_path = Path(artifact.extract_path)
+	src_path = next(unpack_path.iterdir())  # path to `VSCode-linux-x64`
+	sh_paths = [path.relative_to(src_path) for path in src_path.rglob('*.sh')]
+	so_paths = [path.relative_to(src_path) for path in src_path.rglob('*.so')]
+	node_paths = [path.relative_to(src_path) for path in src_path.rglob('*.node')]
+	# rg is for ctrl+shift+f
+	rg = [path.relative_to(src_path) for path in src_path.rglob('rg')]
+	revision = 1 if version == '1.48.2' else 0
 
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
 		version=version,
-		artifacts=[source_artifact],
-		fperms_paths=sh_paths + so_paths,
+		revision=revision,
+		artifacts=[artifact],
+		fperms_paths=sh_paths + so_paths + node_paths + rg,
 		src_path_name=src_path.name,
 	)
 	ebuild.push()

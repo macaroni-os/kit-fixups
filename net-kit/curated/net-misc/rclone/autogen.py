b03262880b2c9d7905b3499afb4b10447cb573de
drobbins <drobbins@funtoo.org>
2020-09-09 18:34:19 +0000

    Merge pull request #717 in CORE/kit-fixups from ~ALEX2101/kit-fixups:bugfix/FL-7458-net-misc-rclone-ebuild-appears-to-be-missing-important-things to master
    Squashed commit of the following:
    
    commit 8418258535be54044855b3d18f3416caf6c4714c
    Author: alex2101 <alex2101@gmail.com>
    Date:   Wed Sep 9 07:58:29 2020 -0300
    
        Revert "Extra debugging for an immodules update failure."
    
        This reverts commit 0916a2b44d20a62d6ea3a772fbea52c175ec6202.
    
    commit 736f00c2641093dc23f0c275dc0b57b19ca563e7
    Author: alex2101 <alex2101@gmail.com>
    Date:   Tue Sep 8 11:55:41 2020 -0300
    
        FL-7458: Migrate net-misc/rclone autogen to go-module.
    
    commit 0916a2b44d20a62d6ea3a772fbea52c175ec6202
    Author: Daniel Robbins <drobbins@funtoo.org>
    Date:   Sun Sep 6 01:34:23 2020 -0600
    
        Extra debugging for an immodules update failure.

--- net-kit/curated/net-misc/rclone/autogen.py
+++ net-kit/curated/net-misc/rclone/autogen.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 
 import json
+import re
+
+async def get_gosum_artifacts(hub,github_user,github_repo,version):
+	gosum_raw = await hub.pkgtools.fetch.get_page(f"https://github.com/{github_user}/{github_repo}/raw/{version}/go.sum")
+	gosum_lines = gosum_raw.split("\n")
+	gosum = ""
+	gosum_artifacts = []
+	for line in gosum_lines:
+		module = line.split()
+		if not len(module):
+			continue
+		gosum = gosum + "\t\"" + module[0] + " " + module[1] + "\"\n"
+		module_path = re.sub("([A-Z]{1})", r"!\1", module[0]).lower()
+		module_ver = module[1].split("/")
+		module_ext = "zip"
+		if "go.mod" in module[1]:
+			module_ext = "mod"
+		module_uri = module_path + "/@v/" + module_ver[0] + "." + module_ext
+		module_file = re.sub("/","%2F",module_uri)
+		gosum_artifacts.append(
+			hub.pkgtools.ebuild.Artifact(url="https://proxy.golang.org/" + module_uri, final_name=module_file)
+		)
+	return dict(gosum=gosum, gosum_artifacts=gosum_artifacts)
+
 
 
 async def generate(hub, **pkginfo):
@@ -9,13 +33,16 @@ async def generate(hub, **pkginfo):
 	json_data = await hub.pkgtools.fetch.get_page(f"https://api.github.com/repos/{github_user}/{github_repo}/tags")
 	json_dict = json.loads(json_data)
 	version = json_dict[0]["name"]
+	artifacts = await get_gosum_artifacts(hub,github_user,github_repo,version)
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
 		version=version[1:],  # `[1:]` slices out the 'v' char from the version
+		gosum = artifacts["gosum"],
 		artifacts=[
 			hub.pkgtools.ebuild.Artifact(
 				url=f"https://github.com/{github_user}/{github_repo}/releases/download/{version}/{github_repo}-{version}.tar.gz"
-			)
+			),
+			*artifacts["gosum_artifacts"]
 		],
 	)
 	ebuild.push()

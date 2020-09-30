8f38f0f0cfd2f6effacdbe2bed52e42be3f0824a
drobbins <drobbins@funtoo.org>
2020-09-30 18:55:25 +0000

    Merge pull request #747 in CORE/kit-fixups from ~ALEX2101/kit-fixups:bugfix/FL-7460-net-libs-libtorrent-rasterbar-wrong-workdir to master
    Squashed commit of the following:
    
    commit 84843eeb6d44942c31af7ae16e93ef4a33ef9015
    Author: alex2101 <alex2101@gmail.com>
    Date:   Wed Sep 30 09:13:35 2020 -0300
    
        FL-7460: Fix net-libs/libtorrent-rasterbar and keep the last working version also.

--- net-kit/curated/net-libs/libtorrent-rasterbar/autogen.py
+++ net-kit/curated/net-libs/libtorrent-rasterbar/autogen.py
@@ -7,20 +7,39 @@ async def generate(hub, **pkginfo):
 	user = "arvidn"
 	repo = "libtorrent"
 	app = pkginfo["name"]
+	last_api_ver = "1.2.10"
+	version = []
+	url = []
 	json_list = await hub.pkgtools.fetch.get_page(f"https://api.github.com/repos/{user}/{repo}/releases", is_json=True)
 	for release in json_list:
 		if release["prerelease"]:
 			continue
 		if release["draft"]:
 			continue
-		version = release["tag_name"]
-		url = release["tarball_url"]
-		break
-	final_name = f"{app}-{version}.tar.gz"
+		version.append(release["tag_name"].rsplit(sep="-")[-1])
+		url.append(
+			list(
+				filter(
+					lambda x: x["browser_download_url"].endswith("tar.gz"),
+					release["assets"]
+				)
+			)[0]["browser_download_url"]
+		)
+	final_name = f"{app}-{version[0]}.tar.gz"
+	last_api_name = f"{app}-{last_api_ver}.tar.gz"
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
-		**pkginfo, version=version, artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)]
+		**pkginfo,
+		version=version[0],
+		artifacts=[hub.pkgtools.ebuild.Artifact(url=url[0], final_name=final_name)]
 	)
 	ebuild.push()
+	last_api_ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		version=last_api_ver,
+		template=app+"-last_api.tmpl",
+		artifacts=[hub.pkgtools.ebuild.Artifact(url=url[version.index(last_api_ver)], final_name=last_api_name)]
+	)
+	last_api_ebuild.push()
 
 
 # vim: ts=4 sw=4 noet

c89c73885ed8e0225b0fe4f7860150bf03746e0f
Daniel Robbins <drobbins@funtoo.org>
2020-10-12 14:45:31 -0600

    Fix slack-bin regex in autogen.

--- net-kit/curated/net-im/slack-bin/autogen.py
+++ net-kit/curated/net-im/slack-bin/autogen.py
@@ -6,14 +6,13 @@ import re
 async def generate(hub, **pkginfo):
 
 	html = await hub.pkgtools.fetch.get_page("https://slack.com/downloads/instructions/debian")
-	match = re.search(r"https://downloads\.slack-edge\.com/linux_releases/slack-desktop-(.\..\..)-amd64.deb", html)
+	match = re.search(r"https://downloads\.slack-edge\.com/linux_releases/slack-desktop-([^-]+)-amd64.deb", html)
 	if match is None:
 		raise hub.pkgtools.ebuild.BreezyError("Couldn't find slack version.")
-	url = match.group(0)  # the entire match
-	version = match.group(1)  # just the version
-	revision = 1 if version == "4.3.2" else 0
+	url = match.group(0)
+	version = match.group(1)
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
-		**pkginfo, version=version, revision=revision, artifacts=[hub.pkgtools.ebuild.Artifact(url=url)]
+		**pkginfo, version=version, artifacts=[hub.pkgtools.ebuild.Artifact(url=url)]
 	)
 
 	ebuild.push()

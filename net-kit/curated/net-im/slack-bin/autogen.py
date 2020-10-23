79ad351e4e8c0b07928ab59409c9363ab5d505e2
Daniel Robbins <drobbins@funtoo.org>
2020-10-23 15:50:18 -0600

    Slack re-did their download page. Also, a lil fix for a python autogen dep.

--- net-kit/curated/net-im/slack-bin/autogen.py
+++ net-kit/curated/net-im/slack-bin/autogen.py
@@ -5,12 +5,13 @@ import re
 
 async def generate(hub, **pkginfo):
 
-	html = await hub.pkgtools.fetch.get_page("https://slack.com/downloads/instructions/debian")
-	match = re.search(r"https://downloads\.slack-edge\.com/linux_releases/slack-desktop-([^-]+)-amd64.deb", html)
+	html = await hub.pkgtools.fetch.get_page("https://slack.com/downloads/linux")
+	url = "https://downloads.slack-edge.com/linux_releases/slack-desktop-{version}-amd64.deb"
+	match = re.search(r"<strong>Version ([0-9.]+)</strong>", html)
 	if match is None:
 		raise hub.pkgtools.ebuild.BreezyError("Couldn't find slack version.")
-	url = match.group(0)
 	version = match.group(1)
+	url = url.format(version=version)
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo, version=version, artifacts=[hub.pkgtools.ebuild.Artifact(url=url)]
 	)

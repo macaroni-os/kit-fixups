3c5233decc02b8af499abba46f45ebb6c50e5813
drobbins <drobbins@funtoo.org>
2020-11-23 19:38:56 +0000

    Merge pull request #895 in CORE/kit-fixups from ~ALEX2101/kit-fixups:bugfix/FL-7365-vulkan-tools-does-not-respect-the-wayland-use-flag to master
    Squashed commit of the following:
    
    commit 2ccffbeb3c01f490d3c32bbc65386b1744db5a33
    Author: alex2101 <alex2101@gmail.com>
    Date:   Mon Nov 23 16:23:00 2020 -0300
    
        FL-7365: Force dev-util/vulkan-headers to build with the same version as media-libs/vulkan-loader.
    
    commit 1b6c1ac1a00ed42714f5346465a8e70dc1567ee0
    Merge: d8a69bb7 6c46bb85
    Author: alex2101 <alex2101@gmail.com>
    Date:   Mon Nov 23 12:53:18 2020 -0300
    
        Merge branch 'master' into bugfix/FL-7365-vulkan-tools-does-not-respect-the-wayland-use-flag
    
    commit d8a69bb71704b27455c289a6edcc4a6801ae66a7
    Merge: 2e452cbe d78d723b
    Author: alex2101 <alex2101@gmail.com>
    Date:   Mon Nov 23 12:51:19 2020 -0300
    
        Merge branch 'master' into bugfix/FL-7365-vulkan-tools-does-not-respect-the-wayland-use-flag
    
    commit 2e452cbe3e986ec52c1a032ef3b5cab579f36337
    Author: alex2101 <alex2101@gmail.com>
    Date:   Mon Nov 23 12:50:21 2020 -0300
    
        FL-7365: Add missing dev-util/vulkan-headers to the autogen framework.
    
    commit d04fe4b51648a46119add901b0f7a62e2d1fc3e6
    Author: alex2101 <alex2101@gmail.com>
    Date:   Mon Nov 23 09:06:05 2020 -0300
    
        FL-7365: Update media-kit/packages.yaml.
    
    commit b146b983118591d72ae490698d49c7026d190c97
    Author: alex2101 <alex2101@gmail.com>
    Date:   Mon Nov 23 08:56:26 2020 -0300
    
        FL-7365: Update dev-util/spirv-tools; Add dev-util/spirv-headers, dev-util/vulkan-tools, media-libs/vulkan-{layers,loader}, media-libs/libplacebo, media-libs/shaderc and media-video/mpv to the autogen framework.

--- core-gl-kit/2.0-release/media-libs/vulkan-loader/autogen.py
+++ core-gl-kit/2.0-release/media-libs/vulkan-loader/autogen.py
@@ -6,28 +6,68 @@ import json
 async def generate(hub, **pkginfo):
 	python_compat = "python3+"
 	github_user = "KhronosGroup"
-	github_repo = "Vulkan-Loader"
-	json_list = await hub.pkgtools.fetch.get_page(
-		f"https://api.github.com/repos/{github_user}/{github_repo}/tags", is_json=True
+	github_repo_l = "Vulkan-Loader"
+	github_repo_h = "Vulkan-Headers"
+	json_list_l = await hub.pkgtools.fetch.get_page(
+		f"https://api.github.com/repos/{github_user}/{github_repo_l}/tags", is_json=True
 	)
-	for tag in json_list:
-		v = tag["name"].lstrip("v")
-		if "-rc" in v or "windows" in v:
-			continue
-		version = v
-		url = tag["tarball_url"]
-		break
-	final_name = f'{pkginfo["name"]}-{version}.tar.gz'
+	json_list_h = await hub.pkgtools.fetch.get_page(
+		f"https://api.github.com/repos/{github_user}/{github_repo_h}/tags", is_json=True
+	)
+	version_h = []
+	url_h = []
 
-	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+	for tag_l in json_list_l:
+		for tag_h in json_list_h:
+			v = tag_h["name"].lstrip("v")
+			if "-rc" in v or "sdk" in v or "windows" in v:
+				continue
+			version_h.append(v)
+			url_h.append(tag_h["tarball_url"])
+		if tag_l["name"].lstrip("v") in version_h:
+			version_l = tag_l["name"].lstrip("v")
+			url_l = tag_l["tarball_url"]
+			break
+	final_name_l = f'{pkginfo["name"]}-{version_l}.tar.gz'
+	final_name_h1 = f'vulkan-headers-{version_l}.tar.gz'
+	final_name_h2 = f'vulkan-headers-{version_h[0]}.tar.gz'
+
+	ebuild_l = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
-		version=version,
+		version=version_l,
 		python_compat=python_compat,
 		github_user=github_user,
-		github_repo=github_repo,
-		artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)]
+		github_repo=github_repo_l,
+		artifacts=[hub.pkgtools.ebuild.Artifact(url=url_l, final_name=final_name_l)]
 	)
-	ebuild.push()
+	ebuild_l.push()
+
+	ebuild_h1 = hub.pkgtools.ebuild.BreezyBuild(
+		template_path=ebuild_l.template_path,
+		cat="dev-util",
+		name="vulkan-headers",
+		version=version_l,
+		python_compat=python_compat,
+		github_user=github_user,
+		github_repo=github_repo_h,
+		artifacts=[hub.pkgtools.ebuild.Artifact(
+			url=url_h[version_h.index(version_l)],
+			final_name=final_name_h1)]
+	)
+	ebuild_h1.push()
+
+	ebuild_h2 = hub.pkgtools.ebuild.BreezyBuild(
+		template_path=ebuild_l.template_path,
+		cat="dev-util",
+		name="vulkan-headers",
+		version=version_h[0],
+		python_compat=python_compat,
+		github_user=github_user,
+		github_repo=github_repo_h,
+		artifacts=[hub.pkgtools.ebuild.Artifact(
+			url=url_h[0],final_name=final_name_h2)]
+	)
+	ebuild_h2.push()
 
 
 # vim: ts=4 sw=4 noet

45c52464c141e11c2ccdf8dc6517188520e14da1
drobbins <drobbins@funtoo.org>
2020-12-14 23:52:45 +0000

    Merge pull request #931 in CORE/kit-fixups from ~INVAKID404/kit-fixups:master to master
    Squashed commit of the following:
    
    commit ccd9a12b75893613b45bd0bd0a280103739bfc8f
    Author: invakid404 <splashgamingbg@gmail.com>
    Date:   Mon Dec 14 15:21:13 2020 +0200
    
        Reformatted with tan
    
    commit cfca185809c36e85466547b958802bff4e9e7840
    Author: invakid404 <splashgamingbg@gmail.com>
    Date:   Mon Dec 14 15:17:47 2020 +0200
    
        Autogenned tree-sitter
    
    commit 09149412312ae34f3aa9cd2412237b400acf4f78
    Author: invakid404 <splashgamingbg@gmail.com>
    Date:   Mon Dec 14 15:16:41 2020 +0200
    
        Autogenned libvterm
    
    commit 127ded08c14b3789e420a34689bc42df77160931
    Author: invakid404 <splashgamingbg@gmail.com>
    Date:   Mon Dec 14 15:16:10 2020 +0200
    
        Forked libvterm
    
    commit 50a8aff4286b34ac0df1edb33ca846569cce4951
    Author: invakid404 <splashgamingbg@gmail.com>
    Date:   Mon Dec 14 15:15:13 2020 +0200
    
        Autogenned luv
    
    commit 8c256762ba77e44209675927dacbca7d23d97fa8
    Author: invakid404 <splashgamingbg@gmail.com>
    Date:   Mon Dec 14 15:13:20 2020 +0200
    
        Autogenned neovim
    
    commit 374f0f18244f4254ff84053f7f718fc8be627683
    Author: invakid404 <splashgamingbg@gmail.com>
    Date:   Mon Dec 14 15:12:30 2020 +0200
    
        Forked neovim

---
+++ editors-kit/curated/app-editors/neovim/autogen.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import re
+
+
+def get_release(release_data, is_stable=True):
+	releases = list(filter(lambda x: x["prerelease"] != is_stable, release_data))
+	return None if not releases else sorted(releases, key=lambda x: x["tag_name"]).pop()
+
+
+def generate_ebuild(hub, repo_name, release_data, is_stable, **pkginfo):
+	curr_release = get_release(release_data, is_stable)
+	if curr_release is None:
+		raise hub.pkgtools.ebuild.BreezyError(f"Can't find a suitable release of {repo_name}")
+	tag = curr_release["tag_name"]
+	# version = curr_release["name"].split(" ")[-1].split("-")[0].lstrip("v")
+	version = re.sub("[^0-9.]", "", curr_release["name"])
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		version=version,
+		stable=is_stable,
+		artifacts=[
+			hub.pkgtools.ebuild.Artifact(
+				url=f"https://github.com/{repo_name}/{repo_name}/archive/{tag}.tar.gz",
+				final_name=f"{repo_name}-{version}.tar.gz",
+			)
+		],
+	)
+	ebuild.push()
+
+
+async def generate(hub, **pkginfo):
+	name = pkginfo["name"]
+	release_data = await hub.pkgtools.fetch.get_page(
+		f"https://api.github.com/repos/{name}/{name}/releases", is_json=True
+	)
+
+	generate_ebuild(hub, name, release_data, True, **pkginfo)
+	generate_ebuild(hub, name, release_data, False, **pkginfo)

ac82da97c357b46a75a52fc9d43b0d5ac9538cbd
alex2101 <alex2101@gmail.com>
2021-02-16 10:42:08 +0000

    Merge pull request #1091 in CORE/kit-fixups from ~INVAKID404/kit-fixups:bugfix/FL-8087-kitty-fails-to-compile-docs-after-sphinx-update to master
    Squashed commit of the following:
    
    commit d3eb4bb0ad8aae254f8ab53812a6f9e4cedf43c3
    Author: invakid404 <splashgamingbg@gmail.com>
    Date:   Tue Feb 16 09:48:46 2021 +0200
    
        Format with tan
    
    commit 3be2c360d82b9c7d70d648f11b8b402d9299eb82
    Author: invakid404 <splashgamingbg@gmail.com>
    Date:   Tue Feb 16 09:48:19 2021 +0200
    
        Bring back packaging.version
    
    commit 3545eea8455047f3e8178c89694a84e60c8d9e3c
    Author: invakid404 <splashgamingbg@gmail.com>
    Date:   Tue Feb 16 09:47:36 2021 +0200
    
        Fix patches
    
    commit 4b7f9b356e46b7dada909fe4566155af3b7065d5
    Author: invakid404 <splashgamingbg@gmail.com>
    Date:   Tue Feb 16 09:46:41 2021 +0200
    
        Converted kitty to a commit-based autogen; Sedified some problematic patches

--- desktop-kit/curated/x11-terms/kitty-gen/generator.py
+++ desktop-kit/curated/x11-terms/kitty-gen/generator.py
@@ -1,26 +1,57 @@
 #!/usr/bin/env python3
-
+from datetime import datetime, timedelta
 from packaging import version
 
 
+async def query_github_api(user, repo, query):
+	return await hub.pkgtools.fetch.get_page(
+		f"https://api.github.com/repos/{user}/{repo}/{query}",
+		is_json=True,
+		refresh_interval=timedelta(days=15),
+	)
+
+
 def get_release(release_data):
-	releases = list(filter(lambda x: x["prerelease"] is False and x["draft"] is False, release_data))
-	return None if not releases else sorted(releases, key=lambda x: version.parse(x["tag_name"])).pop()
+	releases = list(
+		filter(lambda x: x["prerelease"] is False and x["draft"] is False, release_data)
+	)
+	return (
+		None
+		if not releases
+		else sorted(releases, key=lambda x: version.parse(x["tag_name"])).pop()
+	)
+
+
+async def is_commit_safe(user, repo, commit):
+	commit_status = await query_github_api(user, repo, f"commits/{commit}/status")
+	return "failure" not in commit_status["state"]
 
 
 async def generate(hub, **pkginfo):
 	user = "kovidgoyal"
 	repo = "kitty"
-	releases_data = await hub.pkgtools.fetch.get_page(
-		f"https://api.github.com/repos/{user}/{repo}/releases", is_json=True
-	)
+	releases_data = await query_github_api(user, repo, "releases")
 	latest_release = get_release(releases_data)
 	if latest_release is None:
 		raise hub.pkgtools.ebuild.BreezyError(f"Can't find a suitable release of {repo}")
 	version = latest_release["tag_name"]
+	commits_data = await query_github_api(user, repo, "commits")
+	safe_commits = (
+		commit for commit in commits_data if await is_commit_safe(user, repo, commit["sha"])
+	)
+	target_commit = await safe_commits.__anext__()
+	commit_date = datetime.strptime(
+		target_commit["commit"]["committer"]["date"], "%Y-%m-%dT%H:%M:%SZ"
+	)
+	commit_hash = target_commit["sha"]
+	version += "." + commit_date.strftime("%Y%m%d")
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
 		version=version.lstrip("v"),
-		artifacts=[hub.pkgtools.ebuild.Artifact(url=f"https://github.com/{user}/{repo}/archive/{version}.tar.gz")],
+		artifacts=[
+			hub.pkgtools.ebuild.Artifact(
+				url=f"https://github.com/{user}/{repo}/archive/{commit_hash}.tar.gz"
+			)
+		],
 	)
 	ebuild.push()

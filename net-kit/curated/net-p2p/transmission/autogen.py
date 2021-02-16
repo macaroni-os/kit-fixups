c986371b5392bebbd91d924d0f24e7a8b127608c
alex2101 <alex2101@gmail.com>
2021-02-16 14:15:17 +0000

    Merge pull request #1090 in CORE/kit-fixups from ~KRISH/krish-kit-fixups:bugfix/FL-8057-update-net-p2p-transmission-to-latest-version to master
    Squashed commit of the following:
    
    commit bc8656d43c25c640d0cd2ac1776ccdb9cca66b3e
    Author: krish <krishenning@gmail.com>
    Date:   Tue Feb 16 05:54:28 2021 -0500
    
        FL-8057: update net-p2p/transmission to latest fixed autogen2
    
    commit 7477ba7a74299abaf99c448a2cb343818ef48e89
    Author: krish <krishenning@gmail.com>
    Date:   Tue Feb 16 05:52:31 2021 -0500
    
        FL-8057: update net-p2p/transmission to latest fixed autogen
    
    commit c25f3f1835e6e15385990ebc323f76c291ecf09f
    Author: krish <krishenning@gmail.com>
    Date:   Mon Feb 15 21:18:45 2021 -0500
    
        FL-8057: Deleted unnecessary file
    
    commit 37fa41d87a12dfc550f64b4c33f3f75b0fe57852
    Author: krish <krishenning@gmail.com>
    Date:   Mon Feb 15 20:59:21 2021 -0500
    
        FL-8057: update net-p2p/transmission to latest version

---
+++ net-kit/curated/net-p2p/transmission/autogen.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import json
+from packaging import version
+
+async def generate(hub, **pkginfo):
+	github_user = "transmission"
+	github_repo = "transmission"
+	app = pkginfo["name"]
+	json_list = await hub.pkgtools.fetch.get_page(
+		f"https://api.github.com/repos/{github_user}/{github_repo}/releases", is_json=True,
+	)
+
+	json_list = sorted(json_list, key=lambda x: version.parse(x["tag_name"]), reverse=True)
+
+	for release in json_list:
+		if release["prerelease"] or release["draft"]:
+			continue
+		tag_name = release["tag_name"][0:]
+		break
+
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		version=tag_name,
+		github_user=github_user,
+		github_repo=github_repo,
+		artifacts=[hub.pkgtools.ebuild.Artifact(url=f"https://github.com/transmission/transmission-releases/raw/master/{app}-{tag_name}.tar.xz", final_name=f"{app}-{tag_name}.tar.xz")],
+	)
+	ebuild.push()
+
+# vim: ts=4 sw=4 noet

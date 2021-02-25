96d3d7ea55b93f25488bb0f7802c80d89c8aa603
Daniel Robbins <drobbins@funtoo.org>
2021-02-24 22:25:12 -0700

    FL-8119: support alternate Slack web pages for version scraping.

--- net-kit/curated/net-im/slack-bin/autogen.py
+++ net-kit/curated/net-im/slack-bin/autogen.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python3
 
-import re
-
+from bs4 import BeautifulSoup
 
 async def generate(hub, **pkginfo):
-
 	html = await hub.pkgtools.fetch.get_page("https://slack.com/downloads/linux")
 	url = "https://downloads.slack-edge.com/linux_releases/slack-desktop-{version}-amd64.deb"
-	match = re.search(r"<strong>Version ([0-9.]+)</strong>", html)
-	if match is None:
-		raise hub.pkgtools.ebuild.BreezyError("Couldn't find slack version.")
-	version = match.group(1)
+	soup = BeautifulSoup(html, 'html.parser')
+	link = soup.select_one(".page-downloads__hero__meta-text__version")
+	try:
+		version = None
+		if link is not None:
+			try:
+				link_text = link.get_text()
+				version = link_text.split()[-1]
+			except AttributeError:
+				pass
+		if version is None:
+			print("Using alternative HTML.")
+			div = soup.select_one(".download-meta")
+			div_text = div.get_text()
+			div_ts = div_text.split()
+			if not len(div_ts) or div_ts[0] != "Version":
+				raise AttributeError()
+			version = div_text.split()[1]
+	except AttributeError:
+		with open("foo", "w") as myf:
+			myf.write(html)
+		raise hub.pkgtools.ebuild.BreezyError(f"Found unexpected something in slack-bin html.")
 	url = url.format(version=version)
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo, version=version, artifacts=[hub.pkgtools.ebuild.Artifact(url=url)]

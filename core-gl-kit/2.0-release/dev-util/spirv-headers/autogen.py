93837f980368addc146e05a2e909590e5b681174
alex2101 <alex2101@gmail.com>
2021-02-27 00:52:39 +0000

    Merge pull request #1102 in CORE/kit-fixups from ~ALEX2101/kit-fixups:bugfix/FL-8099-dev-util-spirv-tools-2020.7-core-gl-kit-fails-during-compile-phase to master
    Squashed commit of the following:
    
    commit 772a6bfbbea9d47ca6635ddee704b765c7f14596
    Author: alex2101 <alex2101@gmail.com>
    Date:   Fri Feb 26 21:50:45 2021 -0300
    
        FL-8099: Change dev-util/spirv-headers autogen to commit based.

--- core-gl-kit/2.0-release/dev-util/spirv-headers/autogen.py
+++ core-gl-kit/2.0-release/dev-util/spirv-headers/autogen.py
@@ -1,46 +1,42 @@
 #!/usr/bin/env python3
 
-import json
+from datetime import datetime, timedelta
+
+
+async def query_github_api(user, repo, query):
+	return await hub.pkgtools.fetch.get_page(
+		f"https://api.github.com/repos/{user}/{repo}/{query}",
+		is_json=True,
+		refresh_interval=timedelta(days=15),
+	)
 
 
 async def generate(hub, **pkginfo):
 	github_user = "KhronosGroup"
 	github_repo = "SPIRV-Headers"
-	json_list = await hub.pkgtools.fetch.get_page(
-		f"https://api.github.com/repos/{github_user}/{github_repo}/tags", is_json=True
-	)
-	version_wanted = "1.5.4.raytracing.fixed"
-	url_wanted = None
-	url = None
-	for tag in json_list:
+	json_data = await query_github_api(github_user, github_repo, "tags")
+	for tag in json_data:
 		v = tag["name"].lstrip("v")
-		if version_wanted in v:
-			url_wanted = tag["tarball_url"]
-		if "-rc" in v or "vulkan" in v or url is not None:
+		if "-rc" in v or "vulkan" in v:
 			continue
 		version = v
-		url = tag["tarball_url"]
-	final_name_wanted = f'{pkginfo["name"]}-1.5.4_p1.tar.gz'
+		break
+	commit_data = await query_github_api(github_user, github_repo, "commits/master")
+	commit_hash = commit_data["sha"]
+	commit_date = datetime.strptime(commit_data["commit"]["committer"]["date"], "%Y-%m-%dT%H:%M:%SZ")
+	version += "_p" + commit_date.strftime("%Y%m%d")
+	url = f"https://github.com/{github_user}/{github_repo}/archive/{commit_hash}.tar.gz"
 	final_name = f'{pkginfo["name"]}-{version}.tar.gz'
-
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
-		version=version,
 		github_user=github_user,
 		github_repo=github_repo,
-		artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)],
+		version=version,
+		artifacts=[
+			hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name),
+		],
 	)
 	ebuild.push()
-
-	if url_wanted != None:
-		ebuild = hub.pkgtools.ebuild.BreezyBuild(
-			**pkginfo,
-			version="1.5.4_p1",
-			github_user=github_user,
-			github_repo=github_repo,
-			artifacts=[hub.pkgtools.ebuild.Artifact(url=url_wanted, final_name=final_name_wanted)],
-		)
-		ebuild.push()
 
 
 # vim: ts=4 sw=4 noet

04d3443f87da4c974628f490631771f224560fa5
Daniel Robbins <drobbins@funtoo.org>
2021-03-03 00:10:10 -0700

    FL-8156: adding a workaround for minecraft-launcher autogen. Requires the 'subpop' branch of funtoo-metatools.

--- games-kit/curated/games-action/minecraft-launcher/autogen.py
+++ games-kit/curated/games-action/minecraft-launcher/autogen.py
@@ -5,15 +5,18 @@ async def generate(hub, **pkginfo):
 	changelog_data = await hub.pkgtools.fetch.get_page(
 		"https://launchercontent.mojang.com/launcherPatchNotes_v2.json", is_json=True
 	)
-	version = changelog_data["entries"][0]["versions"]["linux"]
+	for entry in changelog_data["entries"]:
+		version = entry["versions"]["linux"]
+		art = hub.pkgtools.ebuild.Artifact(url=f"https://launcher.mojang.com/download/linux/x86_64/minecraft-launcher_{version}.tar.gz")
+		try:
+			await art.try_fetch()
+			break
+		except hub.pkgtools.fetch.FetchError:
+			continue
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
 		version=version,
-		artifacts=[
-			hub.pkgtools.ebuild.Artifact(
-				url=f"https://launcher.mojang.com/download/linux/x86_64/minecraft-launcher_{version}.tar.gz"
-			),
-		],
+		artifacts=[art]
 	)
 	ebuild.push()
 

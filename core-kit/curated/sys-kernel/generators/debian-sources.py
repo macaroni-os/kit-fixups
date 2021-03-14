bb89d6a607fdea3e67453565cc6f0863329ccc19
drobbins <drobbins@funtoo.org>
2021-03-14 19:08:35 +0000

    Merge pull request #1114 in CORE/kit-fixups from ~INVAKID404/kit-fixups:debian-sources-autogen to master
    Squashed commit of the following:
    
    commit 8302fb353a89d260199f23e54675481f6747e69c
    Author: invakid404 <splashgamingbg@gmail.com>
    Date:   Thu Mar 4 11:40:05 2021 +0200
    
        Added patches
    
    commit a06e13841a6628e3f7d618d49c1b98aa807d91dd
    Author: invakid404 <splashgamingbg@gmail.com>
    Date:   Thu Mar 4 11:36:23 2021 +0200
    
        Set extra version
    
    commit cabbbf63061ef8602365da7dfe2fd79409de48b5
    Author: invakid404 <splashgamingbg@gmail.com>
    Date:   Thu Mar 4 10:48:22 2021 +0200
    
        Correct workdir
    
    commit f41ab3d5d4ddbfec65ef6bbff69f262d26f23f37
    Author: invakid404 <splashgamingbg@gmail.com>
    Date:   Thu Mar 4 09:11:11 2021 +0200
    
        Remove unneeded import
    
    commit dae7e130409fbbc58ac1592d8dae64fe68364236
    Author: invakid404 <splashgamingbg@gmail.com>
    Date:   Thu Mar 4 00:03:46 2021 +0200
    
        Revert "Look for the source tarball in the release"
    
        This reverts commit 88a04eebc42fdcf773d886a14d516443f97e1dcf.
    
    commit f8cde9e179c8e200595cfe6a98bfadf60ced0328
    Author: invakid404 <splashgamingbg@gmail.com>
    Date:   Thu Mar 4 00:01:12 2021 +0200
    
        Generate ebuilds for debian release kernel versions
    
    commit 88a04eebc42fdcf773d886a14d516443f97e1dcf
    Author: invakid404 <splashgamingbg@gmail.com>
    Date:   Mon Mar 1 23:31:11 2021 +0200
    
        Look for the source tarball in the release

--- core-kit/curated/sys-kernel/generators/debian-sources.py
+++ core-kit/curated/sys-kernel/generators/debian-sources.py
@@ -1,15 +1,37 @@
 #!/usr/bin/python3
 
+from bs4 import BeautifulSoup
+
 GLOBAL_DEFAULTS = {}
+generated_versions = dict()
+
+
+async def get_version_for_release(release_name):
+	tracker_data = await hub.pkgtools.fetch.get_page("https://tracker.debian.org/pkg/linux")
+	tracker_soup = BeautifulSoup(tracker_data, "lxml")
+	target_release = next(x for x in tracker_soup.find_all("span", class_="versions-repository") if x.text.strip()[:-1] == release_name)
+	return target_release.parent.find("a").text.split("-")
 
 
 async def generate(hub, **pkginfo):
+	release_prefix = "deb-"
+	deb_pv_base = pkginfo.get("deb_pv_base")
+	deb_extraversion = pkginfo.get("deb_extraversion")
+	if deb_pv_base.startswith(release_prefix):
+		deb_pv_base, deb_extraversion = await get_version_for_release(deb_pv_base.lstrip(release_prefix))
 	base_url = f"http://http.debian.net/debian/pool/main/l/linux"
-	deb_pv = f"{pkginfo['deb_pv_base']}-{pkginfo['deb_extraversion']}"
-	k_artifact = hub.pkgtools.ebuild.Artifact(url=f"{base_url}/linux_{pkginfo['deb_pv_base']}.orig.tar.xz")
+	deb_pv = f"{deb_pv_base}-{deb_extraversion}"
+	k_artifact = hub.pkgtools.ebuild.Artifact(url=f"{base_url}/linux_{deb_pv_base}.orig.tar.xz")
 	p_artifact = hub.pkgtools.ebuild.Artifact(url=f"{base_url}/linux_{deb_pv}.debian.tar.xz")
-	ebuild = hub.pkgtools.ebuild.BreezyBuild(**pkginfo, artifacts=[k_artifact, p_artifact])
-	ebuild.push()
+	version = pkginfo["version"] = f"{deb_pv_base}_p{deb_extraversion}"
+	pkginfo["deb_extraversion"] = deb_extraversion
+	unmasked = pkginfo["unmasked"]
+	global generated_versions
+	prev_result = generated_versions.get(version, None)
+	if prev_result is None or not prev_result and unmasked:
+		ebuild = hub.pkgtools.ebuild.BreezyBuild(**pkginfo, linux_version=deb_pv_base, artifacts=[k_artifact, p_artifact])
+		ebuild.push()
+		generated_versions[version] = unmasked
 
 
 # vim: ts=4 sw=4 noet

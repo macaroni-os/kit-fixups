1508b673133fa3dcd6bf0adad0efad5e9485b1a1
drobbins <drobbins@funtoo.org>
2021-03-27 04:33:37 +0000

    Merge pull request #1165 in CORE/kit-fixups from ~RANDY/kit-fixups:FL-8230 to master
    Squashed commit of the following:
    
    commit 2f843c6fb8372314790146077b2bbc7cc87790bd
    Author: Randy McCaskill <randy@mccaskill.us>
    Date:   Fri Mar 26 22:02:57 2021 -0400
    
        FL-8230: Create autogen file for sys-libs/timezone-data

---
+++ core-hw-kit/curated/sys-libs/timezone-data/autogen.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+import re
+
+async def generate(hub, **pkginfo):
+	url = f"https://www.iana.org/time-zones"
+	html_data = await hub.pkgtools.fetch.get_page(url)
+	soup = BeautifulSoup(html_data, "html.parser")
+	best_tzdata = None
+	pattern = re.compile(".*tzdata(.*)\.tar\.gz")
+	for link in soup.find_all("a"):
+		href = link.get("href")
+		if (pattern.match(href)):
+			best_tzdata = href
+			break
+	version = pattern.match(best_tzdata).group(1)
+	best_tzcode = re.sub(r'tzdata', 'tzcode', best_tzdata)
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo, version=version, artifacts=[hub.pkgtools.ebuild.Artifact(url=best_tzdata),
+											   hub.pkgtools.ebuild.Artifact(url=best_tzcode)]
+	)
+	ebuild.push()
+
+
+# vim: ts=4 sw=4 noet

68bc6187636aba08828c92c19cd8c02a37f5c4d9
alex2101 <alex2101@gmail.com>
2021-04-21 11:18:30 +0000

    Merge pull request #1223 in CORE/kit-fixups from ~INVAKID404/kit-fixups:improvement/FL-8316-vscode-insiders-is-missing to master
    Squashed commit of the following:
    
    commit 6df8cd706b1a65eac9b873843de6b707630cb272
    Author: invakid404 <invakid404@riseup.net>
    Date:   Wed Apr 21 00:16:24 2021 +0300
    
        Retab
    
    commit 3909dd5cdb17c06a684a811c06a137c60b2b8b2e
    Author: invakid404 <invakid404@riseup.net>
    Date:   Wed Apr 21 00:15:27 2021 +0300
    
        Conditionally mask or unmask ebuilds
    
    commit 9e78fe18af58f22c9539ea2d0c967564388c6eaa
    Author: invakid404 <invakid404@riseup.net>
    Date:   Wed Apr 21 00:15:15 2021 +0300
    
        Use the artifact source URL
    
    commit 328aa51ff9ab9442a971b7b8b3c6781418c40d24
    Author: invakid404 <invakid404@riseup.net>
    Date:   Wed Apr 21 00:13:18 2021 +0300
    
        Determine executable name based on release channel
    
        This patch fixes an issue with the ebuild, which was caused by the
        fact that the insiders build's binaries were named `code-insiders`
        while the stable build's binaries were named `code`. This is easily
        resolved by passing that info from the autogen and using it as the
        MY_EXEC environment variable.
    
    commit e292b45dc600a2285dea7a5e38f58d36788e2d85
    Author: invakid404 <invakid404@riseup.net>
    Date:   Wed Apr 21 00:09:46 2021 +0300
    
        Rewrite vscode-bin autogen to generate an insiders ebuild
    
        Since I couldn't find an insiders changelog to use, similarly to
        what we used to do for the stable build, I looked for another way
        to obtain version information.
    
        I figured out that the .tar.gz filename doesn't tell us anything,
        but the .deb file does! The .deb file contains the version in the
        filename for both stable and insiders. So, by resolving the .deb
        download link, I can extract the version via regex and simply use
        it alongside the .tar.gz download link.
    
        I decided it's a good idea to resolve the .tar.gz URL too, since
        simply using the direct link might cause version mismatches.

--- editors-kit/curated/app-editors/vscode-bin/autogen.py
+++ editors-kit/curated/app-editors/vscode-bin/autogen.py
@@ -4,32 +4,36 @@ import re
 from pathlib import Path
 
 
-async def generate(hub, **pkginfo):
-	raw_html = await hub.pkgtools.fetch.get_page("https://code.visualstudio.com/updates/")
-	versions = re.findall(r"(https://update\.code\.visualstudio\.com/([0-9\.]+)/linux\-x64/stable)", raw_html)
-	if len(versions) != 1:
-		raise hub.pkgtools.ebuild.BreezyError(
-			"vscode page was expected to have exactly one linux download link. The script needs updating"
-		)
-	url, version = versions[0]
-	artifact = hub.pkgtools.ebuild.Artifact(url=url, final_name=f"vscode-bin-{version}.tar.gz")
+async def generate_for(hub, pkg_name, release_channel, unmasked, **pkginfo):
+	url_template = f"https://code.visualstudio.com/sha/download?build={release_channel}&os="
+	redirect_url = await hub.pkgtools.fetch.get_url_from_redirect(url_template + "linux-deb-x64")
+	deb_filename = redirect_url.split("/")[-1]
+	filename_pattern = re.compile(f"^{pkg_name}_([\d.]+)-\d+_amd64\.deb$")
+	version, = filename_pattern.match(deb_filename).groups()
+	src_url = await hub.pkgtools.fetch.get_url_from_redirect(url_template + "linux-x64")
+	artifact = hub.pkgtools.ebuild.Artifact(src_url, final_name=f"{pkginfo['name']}-{version}.tar.gz")
 	await artifact.fetch()
 	artifact.extract()
 	unpack_path = Path(artifact.extract_path)
-	src_path = next(unpack_path.iterdir())  # path to `VSCode-linux-x64`
+	src_path = next(unpack_path.iterdir())	# path to `VSCode-linux-x64`
 	sh_paths = [path.relative_to(src_path) for path in src_path.rglob("*.sh")]
 	so_paths = [path.relative_to(src_path) for path in src_path.rglob("*.so")]
 	node_paths = [path.relative_to(src_path) for path in src_path.rglob("*.node")]
 	# rg is for ctrl+shift+f
 	rg = [path.relative_to(src_path) for path in src_path.rglob("rg")]
-	revision = 1 if version == "1.48.2" else 0
 	artifact.cleanup()
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
 		version=version,
-		revision=revision,
+		channel=pkg_name,
+		unmasked=unmasked,
 		artifacts=[artifact],
 		fperms_paths=sh_paths + so_paths + node_paths + rg,
 		src_path_name=src_path.name,
 	)
 	ebuild.push()
+
+
+async def generate(hub, **pkginfo):
+	await generate_for(hub, "code", "stable", True, **pkginfo)
+	await generate_for(hub, "code-insiders", "insider", False, **pkginfo)

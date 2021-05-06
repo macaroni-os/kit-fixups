5fab5a84cfc4f19ef0225044140f8017b19c741e
alex2101 <alex2101@gmail.com>
2021-05-06 00:37:53 +0000

    Merge pull request #1260 in CORE/kit-fixups from ~ALEX2101/kit-fixups:FL-8285-add-net-misc-teamviewer-v15-to-tree to master
    Squashed commit of the following:
    
    commit 204be9103fdd441b0a15700635a6e25f1389ae07
    Author: alex2101 <alex2101@gmail.com>
    Date:   Wed May 5 21:36:34 2021 -0300
    
        FL-8285: Rework net-misc/teamviewer autogen and template to create a dict artifacts example.

--- net-kit/curated/net-misc/teamviewer/autogen.py
+++ net-kit/curated/net-misc/teamviewer/autogen.py
@@ -5,13 +5,12 @@ import asyncio
 async def generate(hub, **pkginfo):
 
 	url = await hub.pkgtools.fetch.get_url_from_redirect("https://download.teamviewer.com/download/linux/teamviewer_amd64.tar.xz")
-	version = url.split("/")[-1].lstrip("teamviewer_").rstrip("amd64.tar.xz").replace("_", "")
+	version = url.split("_")[-2]
 
-	artifacts = [
-		hub.pkgtools.ebuild.Artifact(url=url, final_name=f"teamviewer_{version}_amd64.tar.xz"),
-		hub.pkgtools.ebuild.Artifact(url=url.replace("amd64", "i386"), final_name=f"teamviewer_{version}_i386.tar.xz"),
-	]
+	artifacts = {
+		"amd64": hub.pkgtools.ebuild.Artifact(url=url),
+		"x86": hub.pkgtools.ebuild.Artifact(url=url.replace("amd64", "i386")),
+	}
 
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(**pkginfo, version=version, artifacts=artifacts)
-
 	ebuild.push()

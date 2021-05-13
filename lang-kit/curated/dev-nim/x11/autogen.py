77eedd32e2eb5f336fee468feec03c6686a03083
alex2101 <alex2101@gmail.com>
2021-05-13 20:56:35 +0000

    Merge pull request #1271 in CORE/kit-fixups from ~SORTHAK/kit-fixups:master to master
    Squashed commit of the following:
    
    commit 8301043c8cce923c2a49e89bac8333d2048ad2d9
    Author: sorthak <sorthak@t-online.de>
    Date:   Wed May 12 22:19:05 2021 +0200
    
        FL-8313: add Nimdow including 2 required packages, all with autogen

---
+++ lang-kit/curated/dev-nim/x11/autogen.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from datetime import datetime, timedelta
+
+async def generate(hub, **pkginfo):
+	github_user = "nim-lang"
+	github_repo = pkginfo["name"]
+	commit_data = await hub.pkgtools.fetch.get_page(f"https://api.github.com/repos/{github_user}/{github_repo}/commits/master", is_json=True, refresh_interval=timedelta(days=15))
+	commit_hash = commit_data["sha"]
+	commit_date = datetime.strptime(commit_data["commit"]["committer"]["date"], "%Y-%m-%dT%H:%M:%SZ")
+	version = commit_date.strftime("%Y%m%d")
+	final_name = f"{github_repo}-{version}.tar.gz"
+	src_url = f"https://github.com/{github_user}/{github_repo}/archive/{commit_hash}.tar.gz"
+	src_artifact = hub.pkgtools.ebuild.Artifact(url=src_url, final_name=final_name)
+
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		version=version.lstrip("v"),
+		github_repo=github_repo,
+		artifacts=[src_artifact],
+	)
+
+	ebuild.push()
+

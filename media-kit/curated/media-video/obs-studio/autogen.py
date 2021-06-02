e763810e510419166384b45788288b98cb4b44ce
krish <krishenning@gmail.com>
2021-06-02 20:02:51 +0000

    Merge pull request #1289 in CORE/kit-fixups from ~INVAKID404/kit-fixups:bugfix/FL-8459-media-video-obs-studio-outdated-template to master
    Squashed commit of the following:
    
    commit c82307bf18fd0a5834cc774c4fff0b531e297513
    Author: invakid404 <invakid404@riseup.net>
    Date:   Wed Jun 2 21:14:58 2021 +0300
    
        Disable obs-vst
    
    commit dd8af2f2a1580013a6c3deda28d580ccc55adf9c
    Author: invakid404 <invakid404@riseup.net>
    Date:   Wed Jun 2 21:12:24 2021 +0300
    
        Add support for obs-browser
    
    commit 959d8cdefeafaacedbc3fc286ebedee5d2c04a5d
    Author: invakid404 <invakid404@riseup.net>
    Date:   Wed Jun 2 20:51:02 2021 +0300
    
        Add support for pipewire

--- media-kit/curated/media-video/obs-studio/autogen.py
+++ media-kit/curated/media-video/obs-studio/autogen.py
@@ -19,12 +19,26 @@ async def generate(hub, **pkginfo):
 		url = release["tarball_url"]
 		break
 	final_name = f'{pkginfo["name"]}-{version}.tar.gz'
+	src_artifact = hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)
+	browser_repo = "obs-browser"
+	browser_commits = await hub.pkgtools.fetch.get_page(
+		f"https://api.github.com/repos/{github_user}/{browser_repo}/commits", is_json=True
+	)
+	browser_target_commit = browser_commits[0]
+	browser_target_hash = browser_target_commit["sha"]
+	browser_url = f"https://github.com/{github_user}/{browser_repo}/archive/{browser_target_hash}.tar.gz"
+	browser_final_name = f"{browser_repo}-{browser_target_hash}.tar.gz"
+	browser_artifact = hub.pkgtools.ebuild.Artifact(url=browser_url)
+	cef_dir = "cef_binary_4280_linux64"
+	cef_url = f"https://cdn-fastly.obsproject.com/downloads/{cef_dir}.tar.bz2"
+	cef_artifact = hub.pkgtools.ebuild.Artifact(url=cef_url)
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
 		github_user=github_user,
 		github_repo=github_repo,
 		version=version,
-		artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)],
+		cef_dir=cef_dir,
+		artifacts=[src_artifact, browser_artifact, cef_artifact],
 	)
 	ebuild.push()
 

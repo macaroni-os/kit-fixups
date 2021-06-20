347defe200f87e76b0cae1d0a9d785c7ae7e9686
krish <krishenning@gmail.com>
2021-06-20 15:49:16 +0000

    Merge pull request #1330 in CORE/kit-fixups from ~ALEX2101/kit-fixups:bugfix/FL-8547-dev-util-spirv-tools-spv_operand_type_quantization_modes-was-not-declared-in-this to master
    Squashed commit of the following:
    
    commit 4b7c4ecbc9d8762fd7be7314aca7a6eb3c3b41aa
    Author: alex2101 <alex2101@gmail.com>
    Date:   Sun Jun 20 12:11:39 2021 -0300
    
        FL-8547: Couple dev-util/spirv-{headers,tools} autogens.

--- core-gl-kit/2.0-release/dev-util/spirv-tools/autogen.py
+++ core-gl-kit/2.0-release/dev-util/spirv-tools/autogen.py
@@ -1,33 +1,77 @@
 #!/usr/bin/env python3
 
-import json
+import glob
+import os
+from datetime import datetime
 
 
-async def generate(hub, **pkginfo):
-	python_compat = "python3+"
-	github_user = "KhronosGroup"
-	github_repo = "SPIRV-Tools"
-	json_list = await hub.pkgtools.fetch.get_page(
-		f"https://api.github.com/repos/{github_user}/{github_repo}/tags", is_json=True
+async def query_github_api(user, repo, query):
+	return await hub.pkgtools.fetch.get_page(
+		f"https://api.github.com/repos/{user}/{repo}/{query}",
+		is_json=True,
 	)
-	for tag in json_list:
+
+def get_version(json):
+	for tag in json:
 		v = tag["name"].lstrip("v")
 		if "-rc" in v or "vulkan" in v:
 			continue
 		version = v
 		url = tag["tarball_url"]
 		break
-	final_name = f'{pkginfo["name"]}-{version}.tar.gz'
+	return version, url
 
-	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+async def generate(hub, **pkginfo):
+	python_compat = "python3+"
+	github_user = "KhronosGroup"
+	github_repo = "SPIRV-Tools"
+	json_data = await query_github_api(github_user, github_repo, "tags")
+	version, url = get_version(json_data)
+	final_name = f'{pkginfo["name"]}-{version}.tar.gz'
+	tools_artifact = hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)
+
+	tools_ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
-		version=version,
 		python_compat=python_compat,
 		github_user=github_user,
 		github_repo=github_repo,
-		artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)],
+		version=version,
+		artifacts=[tools_artifact],
 	)
-	ebuild.push()
+	tools_ebuild.push()
+
+	await tools_artifact.fetch()
+	tools_artifact.extract()
+	deps_file = open(
+		glob.glob(os.path.join(tools_artifact.extract_path, f"{github_user}-{github_repo}-*", "DEPS"))[0]
+	).readlines()
+
+	for line in deps_file:
+		if "'spirv_headers_revision':" in line:
+			commit_hash = line.split("'")[-2]
+	tools_artifact.cleanup()
+
+	github_repo = "SPIRV-Headers"
+	json_data = await query_github_api(github_user, github_repo, "tags")
+	version, url = get_version(json_data)
+
+	commit_data = await query_github_api(github_user, github_repo, "commits/"+commit_hash)
+	commit_date = datetime.strptime(commit_data["commit"]["committer"]["date"], "%Y-%m-%dT%H:%M:%SZ")
+	version += "_p" + commit_date.strftime("%Y%m%d")
+	url = f"https://github.com/{github_user}/{github_repo}/archive/{commit_hash}.tar.gz"
+	final_name = f'{pkginfo["name"]}-{version}.tar.gz'
+	headers_ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		template_path=tools_ebuild.template_path,
+		cat=pkginfo["cat"],
+		name="spirv-headers",
+		github_user=github_user,
+		github_repo=github_repo,
+		version=version,
+		artifacts=[
+			hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name),
+		],
+	)
+	headers_ebuild.push()
 
 
 # vim: ts=4 sw=4 noet

1ed3820c045edd4fdf936dbfcf2360455fbef23b
Daniel Robbins <drobbins@funtoo.org>
2021-07-20 19:15:07 -0600

    FL-8652: making mongo-tools autogen extremely robust.

--- core-server-kit/curated/app-admin/mongo-tools/autogen.py
+++ core-server-kit/curated/app-admin/mongo-tools/autogen.py
@@ -4,6 +4,8 @@
 # to get the official release version of mongo-tools. Then we go to GitHub to get the sources, because mongo isn't
 # providing an official source tarball. We assume that by the time the version is posted on the Web site, the mongo
 # team has finished messing with their tags.
+#
+# See: FL-8652, FL-8633
 
 from bs4 import BeautifulSoup
 
@@ -18,12 +20,23 @@ async def get_version_from_website(hub, **pkginfo):
 async def generate(hub, **pkginfo):
 	github_user = "mongodb"
 	github_repo = "mongo-tools"
+	
+	# STEP 1: Get released version string from official Web site, because we don't want in-progress not-yet-released tags (a prior issue):
 	ver = await get_version_from_website(hub, **pkginfo)
-	# Note that if we needed to get specific info on this one tag without looking through all tags, we could do:
-	# tag_meta = await hub.pkgtools.fetch.get_page(f"https://api.github.com/repos/{github_user}/{github_repo}/git/ref/tags/{ver}", is_json=True)
-	# tag_info = await hub.pkgtools.fetch.get_page(tag_meta['object']['url'], is_json=True)
-	url = f"https://github.com/{github_user}/{github_repo}/archive/{ver}.tar.gz"
-	final_name = f'{pkginfo["name"]}-{ver}.tar.gz'
+	
+	# STEP 2: Look for GitHub tag with this version, and get its info. Since it's officially released, the tag sha1 shouldn't change:
+	tag_meta = await hub.pkgtools.fetch.get_page(f"https://api.github.com/repos/{github_user}/{github_repo}/git/ref/tags/{ver}", is_json=True)
+	
+	# STEP 3: Now, get the *commit* sha1 from the tag, which also shouldn't change:
+	tag_info = await hub.pkgtools.fetch.get_page(tag_meta['object']['url'], is_json=True)
+	commit_sha1 = tag_meta['object']['sha']
+
+	# STEP 4: Use the commit sha1 to grab the tarball, and name it with part of the SHA1 in the tarball name to avoid conflicts -- just
+	#         in case we're wrong and upstream re-tags -- it will still work! :)
+
+	url = f"https://github.com/{github_user}/{github_repo}/archive/{commit_sha1}.tar.gz"
+	final_name = f'{pkginfo["name"]}-{ver}-{commit_sha1[:8]}.tar.gz'
+
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
 		github_user=github_user,

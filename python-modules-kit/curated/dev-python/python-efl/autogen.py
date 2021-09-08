c0c9ad66f2f4f3865e3faf00646c9c5cdd98da67
invakid404 <splashgamingbg@gmail.com>
2021-09-08 19:41:56 +0000

    Merge pull request #1426 in CORE/kit-fixups from ~NYACODER/kit-fixups:feature/FL-8692-Update-Enlightenment-WM to master
    Squashed commit of the following:
    
    commit 42b016e69772ad8bd1e9a0f414bca51c598a3782
    Author: nyacoder <nyacoder@seleniccoding.com>
    Date:   Wed Sep 8 10:47:05 2021 -0400
    
        Remove tests and get rid of patch in dev-python/python-efl
    
    commit 91ae04e88a6027fa0881304ac9b3b91697c30d05
    Author: nyacoder <nyacoder@seleniccoding.com>
    Date:   Tue Sep 7 11:15:53 2021 -0400
    
        Switch nasm patch to use sed instead
    
    commit 30b40628e56d4cacb009e0ca93e6b144aa523188
    Author: nyacoder <nyacoder@seleniccoding.com>
    Date:   Tue Sep 7 10:27:31 2021 -0400
    
        Update PYTHON_COMPAT
    
    commit e9d550b97df312b82b3cdb27b1331d16584362ca
    Merge: f41718884 1095124d3
    Author: nyacoder <nyacoder@seleniccoding.com>
    Date:   Tue Sep 7 10:19:56 2021 -0400
    
        Merge branch 'master' of https://code.funtoo.org/bitbucket/scm/core/kit-fixups into feature/FL-8692-Update-Enlightenment-WM
    
    commit f417188849bf2734b57a0c7bacf2eabe91dc1c2a
    Author: nyacoder <nyacoder@seleniccoding.com>
    Date:   Tue Sep 7 09:57:15 2021 -0400
    
        Update various packages.yaml files for new autogens
    
    commit 431fb0454c87bf3ab34d04a36ceb217b14a90ca8
    Author: nyacoder <nyacoder@seleniccoding.com>
    Date:   Mon Sep 6 20:23:37 2021 -0400
    
        Add autogen for sys-process/evisum
    
    commit c4359f493e31819d2aeca5a78a747cc72e4e70c3
    Author: nyacoder <nyacoder@seleniccoding.com>
    Date:   Sun Sep 5 09:49:23 2021 -0400
    
        Add autogen for econnman
    
    commit 0429e2541456766728f04ac4e1c97fb5add6825c
    Author: nyacoder <nyacoder@seleniccoding.com>
    Date:   Sun Sep 5 09:44:28 2021 -0400
    
        Add autogen for media-gfx/ephoto
    
    commit 486163a3d7d1f2ee452e5826b9e1e6d95c5939b1
    Author: nyacoder <nyacoder@seleniccoding.com>
    Date:   Sun Sep 5 09:35:24 2021 -0400
    
        Add autogen for dev-util/edi and update enlightenment and efl to use github mirror
    
    commit 72db08b3b08f4bcec6430b162b95444624a45816
    Author: nyacoder <nyacoder@seleniccoding.com>
    Date:   Thu Sep 2 21:55:49 2021 -0400
    
        Add autogen for x11-terms/terminology
    
    commit 91d28a8d28a5f13e2eafb4d19926b2a1481e964b
    Author: nyacoder <nyacoder@seleniccoding.com>
    Date:   Thu Sep 2 21:38:43 2021 -0400
    
        Couple of tweaks for consistency
    
    commit 2dae247c4502c775841f06ae15528ad12eb9da95
    Author: nyacoder <nyacoder@seleniccoding.com>
    Date:   Thu Sep 2 21:35:47 2021 -0400
    
        Add autogen for dev-python/python-efl
    
    commit 55f56bc25b872b4067d67b59ddfb4d08717cb679
    Author: nyacoder <nyacoder@seleniccoding.com>
    Date:   Thu Sep 2 17:35:48 2021 -0400
    
        Add autogen for x11-wm/enlightenment
    
    commit 3663f8c856e608c4b7e54ab86de454f527ec24f9
    Author: nyacoder <nyacoder@seleniccoding.com>
    Date:   Wed Sep 1 12:40:12 2021 -0400
    
        Add autogen for dev-libs/efl
    
    commit 5bb9fef48808873ab6afd38bacdc69f4626c23d4
    Author: nyacoder <nyacoder@seleniccoding.com>
    Date:   Mon Aug 30 22:20:01 2021 -0400
    
        Add autogen for net-libs/grpc
    
    commit 4a517b222d02b68221eedadb2dbad168ca94d2bd
    Author: nyacoder <nyacoder@seleniccoding.com>
    Date:   Mon Aug 30 15:12:30 2021 -0400
    
        Add autogen for media-video/rav1e
    
    commit efcc89c25dbc1cfe12c7c0703e57bc8a26a677d4
    Author: nyacoder <nyacoder@seleniccoding.com>
    Date:   Fri Aug 27 22:00:09 2021 -0400
    
        Add autogen for media-libs/rlottie
    
    commit facdca80b1e32a1c8250ad1c34bc8e3693975d36
    Author: nyacoder <nyacoder@seleniccoding.com>
    Date:   Fri Aug 27 21:58:13 2021 -0400
    
        Add autogen for media-libs/libavif
    
    commit fb8f03c78ad83dd2fadab0348af5805e9e7b0317
    Author: nyacoder <nyacoder@seleniccoding.com>
    Date:   Fri Aug 27 21:56:23 2021 -0400
    
        Add autogen for media-libs/libaom
    
    commit b0e5ece32f28c19b5ba68e73bdee31a4f37417b6
    Author: nyacoder <nyacoder@seleniccoding.com>
    Date:   Fri Aug 27 21:54:31 2021 -0400
    
        Add autogen for dev-util/cargo-c
    
    ... and 8 more commits

---
+++ python-modules-kit/curated/dev-python/python-efl/autogen.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+from packaging import version
+
+
+# We are going to parse the versions from their available downloads since they don't 
+# appear to have a usable rest api.
+# The tags we are interested in all look like this:
+# <a href="pkgname-1.23.3.tar.xz">pkgname-1.23.3.tar.xz</a>
+async def get_version(hub, url, pkgname):
+	page = await hub.pkgtools.fetch.get_page(url)
+	version_list = []
+	# Look for the td class 'indexcolname' and grab the child href to avoid grabbing the icons 
+	# as well since they also contain an href link
+	for td in BeautifulSoup(page, features='html.parser').find_all('td', class_="indexcolname"):
+		href_str = td.find("a", href=True)['href']
+		
+		# Filter out alpha and beta releases along with the sha256 links
+		if "alpha" in href_str or "beta" in href_str or "sha256" in href_str:
+			continue
+		# Filter out anything that doesn't start with pkgname
+		if not href_str.startswith(pkgname):
+			continue
+			
+		# Strip everything but the version number from the string
+		ver_num = href_str.lstrip(f"{pkgname}-").rstrip(".tar.xz")
+		version_list.append(ver_num)
+
+	# Sort the list and return the first one which should be the latest version
+	return None if not version_list else  sorted(version_list, key=lambda x: version.parse(x)).pop()
+
+
+async def generate(hub, **pkginfo):
+	repo_dir = "bindings"
+	url_prefix = f"https://download.enlightenment.org/rel/{repo_dir}/python"
+	ver_num = await get_version(hub, url_prefix, pkginfo['name'])
+	if ver_num is None:
+		raise hub.pkgtools.ebuild.BreezyError(f"Can't find a suitable release of {pkginfo['name']}")
+	url = f"{url_prefix}/{pkginfo['name']}-{ver_num}.tar.xz"
+	src_artifact = hub.pkgtools.ebuild.Artifact(url=url)
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		version=ver_num,
+		artifacts=[src_artifact]
+	)
+	ebuild.push()

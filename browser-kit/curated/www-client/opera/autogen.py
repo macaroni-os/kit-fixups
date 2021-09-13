0291be7e44cd76ed68dc340a44ee77578ccbe909
Daniel Robbins <drobbins@funtoo.org>
2021-09-13 16:11:58 -0600

    FL-8783: robustify opera autogen.

--- browser-kit/curated/www-client/opera/autogen.py
+++ browser-kit/curated/www-client/opera/autogen.py
@@ -9,10 +9,17 @@ async def generate(hub, **pkginfo):
 	src_data = await hub.pkgtools.fetch.get_page(src_url)
 	soup = BeautifulSoup(src_data, "html.parser")
 	best_artifact = None
-	for link in soup.find_all("a"):
+	for link in reversed(soup.find_all("a")):
 		href = link.get("href")
 		best_artifact = href
 		version = best_artifact.rstrip("/")
+		test_url = src_url + f"{version}/linux/"
+		try:
+			# make sure "linux" directory exists and is fetchable -- sometimes a release doesn't have a Linux build:
+			await hub.pkgtools.fetch.get_page(test_url)
+		except hub.pkgtools.fetch.FetchError as fe:
+			continue
+		break
 	url = src_url + f"{version}/linux/{app}-stable_{version}_amd64.deb"
 
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(

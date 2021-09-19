6e8332d95ced29f739813e332eee3a34f05f11fa
Daniel Robbins <drobbins@funtoo.org>
2021-09-19 12:50:59 -0600

    FL-8819: fix xf86-input-libinput by restricting to 1.1.0.

--- core-gl-kit/curated/x11-drivers/generator.py
+++ core-gl-kit/curated/x11-drivers/generator.py
@@ -8,22 +8,23 @@ GLOBAL_DEFAULTS = {"cat": "x11-drivers"}
 async def generate(hub, **pkginfo):
 	name = pkginfo["name"]
 	gitlab_id = pkginfo["gitlab_id"]
-	json_data = await hub.pkgtools.fetch.get_page(
-		f"https://gitlab.freedesktop.org/api/v4/projects/{gitlab_id}/repository/tags"
-	)
-	json_list = json.loads(json_data)
 	if "tag_name" in pkginfo:
 		tag_name = pkginfo["tag_name"]
 	else:
 		tag_name = name
-	version = json_list[0]["name"][len(tag_name) :].lstrip("-")
+	if 'version' not in pkginfo:
+		json_data = await hub.pkgtools.fetch.get_page(
+			f"https://gitlab.freedesktop.org/api/v4/projects/{gitlab_id}/repository/tags"
+		)
+		json_list = json.loads(json_data)
+		pkginfo['version'] = version = json_list[0]["name"][len(tag_name) :].lstrip("-")
+	else:
+		version = pkginfo['version']
 	url = f"https://gitlab.freedesktop.org/xorg/driver/{name}/-/archive/{tag_name}-{version}/{name}-{tag_name}-{version}.tar.bz2"
 	final_name = f"{name}-{version}-gitlab.tar.bz2"
-
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
 		template="driver.tmpl",
-		version=version,
 		artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)],
 	)
 	ebuild.push()

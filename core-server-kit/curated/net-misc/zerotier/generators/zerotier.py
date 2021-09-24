5c5828559950b5ed832c94d2ad7a45b83bfcec9e
Daniel Robbins <drobbins@funtoo.org>
2021-09-24 00:39:06 -0600

    Improve zerotier autogen.

--- core-server-kit/curated/net-misc/zerotier/generators/zerotier.py
+++ core-server-kit/curated/net-misc/zerotier/generators/zerotier.py
@@ -12,15 +12,25 @@ async def generate(hub, **pkginfo):
 	for release in json_data:
 		if release["draft"] is True or release["prerelease"] is True:
 			continue
-		version = release["tag_name"]
+		tag_name = release["tag_name"]
+		version = release["name"].split()[1]
 		break
+	tag_data = await hub.pkgtools.fetch.get_page(
+		f"https://api.github.com/repos/{github_user}/{github_repo}/tags", is_json=True
+	)
+	matching_tag = list(filter(lambda x: x["name"] == tag_name, tag_data))
+	if len(matching_tag) > 1:
+		raise hub.pkgtools.ebuild.BreezyError(f"Found more than one tag for zerotier {tag_name}")
+	elif not len(matching_tag):
+		raise hub.pkgtools.ebuild.BreezyError(f"Could not find matching tag for {tag_name}")
+	matching_tag = matching_tag[0]
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
 		version=version,
 		artifacts=[
 			hub.pkgtools.ebuild.Artifact(
-				url=f"https://api.github.com/repos/zerotier/ZeroTierOne/tarball/{version}",
-				final_name=f"{github_user}-{version}.tar.gz",
+				url=matching_tag["tarball_url"],
+				final_name=f"{github_user}-{tag_name}.tar.gz",
 			)
 		],
 	)

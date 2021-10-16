af2f92a4fe14a5dabce117b55fbd9bf12fd98c73
drobbins <drobbins@funtoo.org>
2021-10-16 15:29:10 +0000

    Merge pull request #1495 in CORE/kit-fixups from FL-8969-add-an-ebuild-for-qownnotes to master
    Squashed commit of the following:
    
    commit deade508fd3585d5ac33768f3bbc79e43035bd44
    Author: invakid404 <invakid404@riseup.net>
    Date:   Sat Oct 16 17:37:27 2021 +0300
    
        Add an autogen for qownnotes

---
+++ desktop-kit/curated/app-office/qownnotes/autogen.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from packaging import version
+from bs4 import BeautifulSoup
+import re
+
+
+async def generate(hub, **pkginfo):
+	tuxfamily_url = "https://download.tuxfamily.org/qownnotes/src"
+	src_pattern = re.compile(f"^({pkginfo.get('name')}-([\\d.]+)\\.tar\\.xz)$")
+
+	tuxfamily_soup = BeautifulSoup(
+		await hub.pkgtools.http.get_page(tuxfamily_url), "lxml"
+	)
+
+	link_matches = (
+		src_pattern.match(link.get("href")) for link in tuxfamily_soup.find_all("a")
+	)
+	valid_matches = (match.groups() for match in link_matches if match)
+
+	target_filename, target_version = max(
+		valid_matches, key=lambda match: version.parse(match[1])
+	)
+	src_url = f"{tuxfamily_url}/{target_filename}"
+
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		version=target_version,
+		artifacts=[hub.pkgtools.ebuild.Artifact(url=src_url)],
+	)
+	ebuild.push()
+

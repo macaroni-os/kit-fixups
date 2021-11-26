175ec3b5c5e07d3e432bd0199bec91554968d4e6
drobbins <drobbins@funtoo.org>
2021-11-26 23:48:15 +0000

    Merge pull request #1540 in CORE/kit-fixups from ~MTAO/kit-fixups:bugfix/FL-9103-openvdb-build-fails to master
    Squashed commit of the following:
    
    commit afbad06ca25a92c85640b06197273c81c412095e
    Author: Michael Tao <mtao@dgp.toronto.edu>
    Date:   Fri Nov 26 11:58:39 2021 -0800
    
        sorting openvdb releases to get the newest

--- media-kit/curated/media-gfx/openvdb/autogen.py
+++ media-kit/curated/media-gfx/openvdb/autogen.py
@@ -3,6 +3,7 @@
 import re
 import glob
 import os
+from packaging import version
 
 async def query_github_api(user, repo, query):
 	return await hub.pkgtools.fetch.get_page(
@@ -10,18 +11,26 @@ async def query_github_api(user, repo, query):
 		is_json=True,
 	)
 
+def get_release_info(releases_data):
+	def prerelease(dat):
+		return dat["prerelease"] or dat["draft"]
+	def get_version(dat):
+		return version.parse(dat["tag_name"].lstrip("v"))
+
+	matches = [x for x in releases_data if not prerelease(x)]
+	releases = sorted((x for x in matches if x), key=get_version)
+	release = releases.pop() if releases else None
+	ver = str(get_version(release))
+	url = release["tarball_url"]
+	return ver, url
 
 async def generate(hub, **pkginfo):
 	github_user = "AcademySoftwareFoundation"
 	github_repo = "openvdb"
 	json_data = await query_github_api(github_user, github_repo, "releases")
-	for release in json_data:
-		if release["prerelease"] or release["draft"]:
-			continue
-		version = release["tag_name"].lstrip("v")
-		url = release["tarball_url"]
-		break
-	final_name = f'{pkginfo["name"]}-{version}.tar.gz'
+	ver,url = get_release_info(json_data)
+
+	final_name = f'{pkginfo["name"]}-{ver}.tar.gz'
 	artifact = hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)
 	await artifact.fetch()
 	artifact.extract()
@@ -33,8 +42,8 @@ async def generate(hub, **pkginfo):
 
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
-		version=version,
-		major_ver=version.split(".")[0],
+		version=ver,
+		major_ver=ver.split(".")[0],
 		github_user=github_user,
 		github_repo=github_repo,
 		abi_min=abi_min,

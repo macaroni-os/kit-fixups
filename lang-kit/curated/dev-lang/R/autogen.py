13ee6c8ffb6c84025cbb425953fdd9650f73e655
drobbins <drobbins@funtoo.org>
2021-12-06 23:02:41 +0000

    Merge pull request #1548 in CORE/kit-fixups from ~MTAO/kit-fixups:bugfix/FL-9145-dev-lang-r-hash-verification-fails to master
    Squashed commit of the following:
    
    commit 5ae965a6f55f474f7e0ee1abdd76da6e44e5a755
    Author: Michael Tao <mtao@dgp.toronto.edu>
    Date:   Mon Dec 6 14:30:14 2021 -0800
    
        switched fetching bashcomp to autogen in R

--- lang-kit/curated/dev-lang/R/autogen.py
+++ lang-kit/curated/dev-lang/R/autogen.py
@@ -3,6 +3,26 @@
 from bs4 import BeautifulSoup
 
 
+async def query_github_api(user, repo, query):
+	return await hub.pkgtools.fetch.get_page(
+		f"https://api.github.com/repos/{user}/{repo}/{query}",
+		is_json=True,
+	)
+
+
+async def get_bashcomp_artifact():
+	github_user = "deepayan"
+	github_repo = "rcompletion"
+
+	commits_data = await query_github_api(github_user,github_repo, "commits")
+	commit_data = commits_data[0]
+
+	bashcomp_sha = commit_data["sha"]
+	final_name = f"{github_repo}-{bashcomp_sha}.tar.gz"
+	src_url = f"https://github.com/{github_user}/{github_repo}/archive/{bashcomp_sha}.tar.gz"
+	return hub.pkgtools.ebuild.Artifact(url=src_url, final_name=final_name), bashcomp_sha
+
+
 async def generate(hub, **pkginfo):
 	url = "https://cran.r-project.org/src/base/R-4/"
 	html_data = await hub.pkgtools.fetch.get_page(url)
@@ -14,8 +34,17 @@ async def generate(hub, **pkginfo):
 			best_archive = href
 	version = best_archive.split(".tar")[0].split("-")[1]
 
+	bcartifact, bashcomp_sha = await get_bashcomp_artifact()
+
+	# fetch bash completion file
+	#hub.pkgtools.ebuild.Artifact(url=
+	#		/{}/bash_completion/R")
+
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
-		**pkginfo, version=version, artifacts=[hub.pkgtools.ebuild.Artifact(url=url + f"{best_archive}")]
+		**pkginfo, version=version, bashcomp_sha=bashcomp_sha, artifacts=[
+			hub.pkgtools.ebuild.Artifact(url=url + f"{best_archive}"),
+			bcartifact
+			]
 	)
 	ebuild.push()
 

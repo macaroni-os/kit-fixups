e22801ecdccd851670788d2f21ed853bc4c96b81
drobbins <drobbins@funtoo.org>
2021-12-06 02:16:21 +0000

    Merge pull request #1546 in CORE/kit-fixups from ~MTAO/kit-fixups:FL-9113-wpebackend-fdo-libwpe-autogen to master
    Squashed commit of the following:
    
    commit 497fd6ff904552c11dd2c97b3c6d19f08513452d
    Author: Michael Tao <mtao@dgp.toronto.edu>
    Date:   Sun Dec 5 11:27:52 2021 -0800
    
        removing unecessary passing of major version to wpe templates
    
    commit 7d82ab13a6b8680314662e525a54a3fd222af78c
    Author: Michael Tao <mtao@dgp.toronto.edu>
    Date:   Sun Dec 5 11:18:26 2021 -0800
    
        adding autogen for libwpe and wpebackend-fdo
        removed patch to add eglmesaext because a prior commit to wpebackend-fdo
        added the desired prototype:
        https://github.com/Igalia/WPEBackend-fdo/commit/b01aa039d1fea67bb0a45cb00d88d28f707c96bb#diff-3e5b3a8102abc40f575e9de4e48cbf8a469b186a626b5091b0a615935905ce1e

---
+++ core-gl-kit/curated/gui-libs/wpebackend-fdo/autogen.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import re
+import glob
+import os
+
+async def query_github_api(user, repo, query):
+	return await hub.pkgtools.fetch.get_page(
+		f"https://api.github.com/repos/{user}/{repo}/{query}",
+		is_json=True,
+	)
+
+
+async def generate(hub, **pkginfo):
+	github_user = "Igalia"
+	github_repo = "WPEBackend-fdo"
+	json_data = await query_github_api(github_user, github_repo, "releases")
+	for release in json_data:
+		if release["prerelease"] or release["draft"]:
+			continue
+		version = release["tag_name"].lstrip("v")
+		url = release["tarball_url"]
+		break
+	final_name = f'{pkginfo["name"]}-{version}.tar.gz'
+	artifact = hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)
+
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		version=version,
+		github_user=github_user,
+		github_repo=github_repo,
+		artifacts=[artifact],
+	)
+	ebuild.push()
+
+
+# vim: ts=4 sw=4 noet

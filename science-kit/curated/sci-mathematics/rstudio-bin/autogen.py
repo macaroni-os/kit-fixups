97763b0201a3c16f02bea7df504f8ee457adec70
drobbins <drobbins@funtoo.org>
2021-12-26 20:11:42 +0000

    Merge pull request #1581 in CORE/kit-fixups from ~MTAO/kit-fixups:FL-9123-update-rstudio-to-version-1.4 to master
    Squashed commit of the following:
    
    commit bf5a1bb97bdf65042e833f0b9e1e3bc2f4d28155
    Author: Michael Tao <mtao@dgp.toronto.edu>
    Date:   Sun Dec 26 11:45:30 2021 -0800
    
        rstudio-bin autogen comments and remove print
    
    commit 7ade0b38e2a9b76f2d0af7e091b85979d62d4893
    Author: Michael Tao <mtao@dgp.toronto.edu>
    Date:   Sun Dec 26 11:43:33 2021 -0800
    
        adding desktop and cleaning up zoom-remainders
    
    commit 9c3f9a14dd1b842d164ae1a215f3159b21e5f635
    Author: Michael Tao <mtao@dgp.toronto.edu>
    Date:   Sun Dec 26 11:15:09 2021 -0800
    
        adding rstudio-bin autogen

---
+++ science-kit/curated/sci-mathematics/rstudio-bin/autogen.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+import re
+
+async def generate(hub, **pkginfo):
+	html_data = await hub.pkgtools.fetch.get_page(
+		f"https://www.rstudio.com/products/rstudio/download/"
+		)
+	soup = BeautifulSoup(html_data, "html.parser")
+	best_archive = None
+	for link in soup.find_all("a"):
+		href = link.get("href")
+		if href is not None and href.endswith(".tar.gz"):
+			# currently the first release is the newest ubuntu/debian based one
+			best_archive = href
+			break
+	version = re.search(r"rstudio-.*-amd64",best_archive).group(0)[8:-6]
+	# comes in form YYYY.MM.DD-NUM and we need to make it YYYY.MM.DD.NUM
+	version = version.replace("-",".")
+
+
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		version=version,
+		artifacts=[hub.pkgtools.ebuild.Artifact(url=best_archive, final_name=f"rstudio-{version}_x86_64.pkg.tar.gz")]
+	)
+
+	ebuild.push()
+
+
+# vim: ts=4 sw=4 noet

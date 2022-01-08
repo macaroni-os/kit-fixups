1d60d78bbf7a6db2b05e7367e8289a39171b9882
Daniel Robbins <drobbins@funtoo.org>
2022-01-08 15:56:48 -0700

    FL-9281: give libreoffice-l10n a fighting chance. This still needs testing.

--- desktop-kit/curated/app-office/libreoffice-bin/autogen.py
+++ desktop-kit/curated/app-office/libreoffice-bin/autogen.py
@@ -1,45 +1,122 @@
 #!/usr/bin/env python3
 
 from bs4 import BeautifulSoup
+import re
 
 
 def get_lang_artifacts(hub, lang_url, lang_soup, lang_type):
-	langs = " "
-	artifacts = []
+	artifacts = {}
 	for link in lang_soup.find_all("a"):
 		href = link.get("href")
 		if lang_type in href and href.endswith("tar.gz"):
 			abbrv = href.split(".tar")[0].split("_")[-1]
+			ver_match = re.search("([0-9.]+)", href)
+			version = ver_match.groups()[0]
 			if "en-US" in abbrv:
 				abbrv = "en:en-US"
 			elif "pa-IN" in abbrv:
 				abbrv = "pa:pa-IN"
 			elif "sa-IN" in abbrv:
 				abbrv = "sa:sa-IN"
-			langs = langs + abbrv + " "
-			artifacts.append(hub.pkgtools.ebuild.Artifact(url=lang_url + href))
-	return dict(langs=langs, artifacts=artifacts)
+			artifacts[abbrv] = hub.pkgtools.ebuild.Artifact(url=lang_url + href)
+			setattr(artifacts[abbrv], 'version', version)
+	return artifacts
+
+
+def gen_src_uri(pkginfo, my_artifacts_dict):
+	"""
+	Given a dictionary mapping language IDs to helppack or langpack artifacts, update 'iuse' in
+	pkginfo and also update 'src_uri' to contain a fully-formatted SRC_URI string to directly
+	insert into the template.
+
+	Input: a dictionary. Each dictionary element has a string language id key that points to the
+	Artifact object represented by the ID (which was grabbed using BeautifulSoup.)
+
+	Output: We will write directly into pkginfo, and not directly return anything. key 'src_uri'
+	will contain the SRC_URI string (which will get appended to in successive calls), and 'iuse'
+	will contain a set of IUSE variables to insert into the template.
+	"""
+	src_uri = ""
+	if 'iuse' not in pkginfo:
+		pkginfo['iuse'] = set()
+	for lang, artifact in my_artifacts_dict.items():
+		lang_first_part = lang.split(':')[0]
+		iuse_lang = f'l10n_{lang_first_part}'
+		pkginfo['iuse'].add(iuse_lang)
+		src_uri += f'{iuse_lang}? ( offlinehelp? ( {artifact.src_uri} ) )\n'
+	if "src_uri" not in pkginfo:
+		pkginfo["src_uri"] = src_uri
+	else:
+		pkginfo["src_uri"] += src_uri
+
+
+def gen_lang_keys(artifacts_dict, main_version):
+	"""
+	This method is used to generate the list of language codes at the top of the template. We have
+	added a feature to optionally store the version of the langpack/helppack at the end of the string
+	like "foo/7.5.2" if the langpack version differs from that in the ebuild. We will then have the
+	bash code in the template grab this version if it exists. We need this for RPM extraction.
+	"""
+	out = []
+	for key in sorted(list(artifacts_dict.keys())):
+		artifact = artifacts_dict[key]
+		if artifact.version != main_version:
+			out.append(f'{key}/{artifact.version}')
+		else:
+			out.append(key)
+	return " ".join(out)
 
 
 async def add_l10n_ebuild(hub, ebuild_bin, version, **pkginfo):
 	dl_url = f"https://downloadarchive.documentfoundation.org/libreoffice/old/{version}/rpm/x86_64/"
 	dl_data = await hub.pkgtools.fetch.get_page(dl_url)
 	dl_soup = BeautifulSoup(dl_data, "html.parser")
-	langpack = get_lang_artifacts(hub, dl_url, dl_soup, "langpack")
-	helppack = get_lang_artifacts(hub, dl_url, dl_soup, "helppack")
-	ebuild_l10n = hub.pkgtools.ebuild.BreezyBuild(
+
+	artifacts = []
+	langpack_artifacts = get_lang_artifacts(hub, dl_url, dl_soup, "langpack")
+	helppack_artifacts = get_lang_artifacts(hub, dl_url, dl_soup, "helppack")
+
+	# Add all artifacts, so we can pass to artifacts= for BreezyBuild so these are all fetched:
+	artifacts += langpack_artifacts.values()
+	artifacts += helppack_artifacts.values()
+
+	gen_src_uri(pkginfo, langpack_artifacts)
+	gen_src_uri(pkginfo, helppack_artifacts)
+
+	# convert iuse from a set to a sorted list for jinja:
+	pkginfo['iuse'] = ' '.join(sorted(list(pkginfo['iuse'])))
+
+	pkginfo.update(
 		template_path=ebuild_bin.template_path,
-		cat=pkginfo["cat"],
 		name="libreoffice-l10n",
 		version=version,
-		languages=langpack["langs"],
-		languages_help=helppack["langs"],
-		artifacts=[
-			*langpack["artifacts"],
-			*helppack["artifacts"],
-		],
+		languages=gen_lang_keys(langpack_artifacts, version),
+		languages_help=gen_lang_keys(helppack_artifacts, version),
+		artifacts=artifacts
 	)
+	ebuild_l10n = hub.pkgtools.ebuild.BreezyBuild(**pkginfo)
 	ebuild_l10n.push()
+
+"""
+	for lang in ${LANGUAGES}; do
+		# break away if not enabled
+		use l10n_${lang%:*} || continue
+
+		dir=${lang#*:}
+
+		# for english we provide just helppack, as translation is always there
+		if [[ ${lang%:*} != en ]]; then
+			rpmdir="LibreOffice_${PV}_Linux_x86-64_rpm_langpack_${dir}/RPMS/"
+			[[ -d ${rpmdir} ]] || die "Missing directory: ${rpmdir}"
+			rpm_unpack ./${rpmdir}/*.rpm
+		fi
+		if [[ "${LANGUAGES_HELP}" =~ " ${lang} " ]] && use offlinehelp; then
+			rpmdir="LibreOffice_${PV}_Linux_x86-64_rpm_helppack_${dir}/RPMS/"
+			[[ -d ${rpmdir} ]] || die "Missing directory: ${rpmdir}"
+			rpm_unpack ./${rpmdir}/*.rpm
+		fi
+	done
+	"""
 
 
 async def generate(hub, **pkginfo):
@@ -53,7 +130,6 @@ async def generate(hub, **pkginfo):
 	dl_url = f"https://downloadarchive.documentfoundation.org/libreoffice/old/{version}/rpm/x86_64/"
 	url = dl_url + f"LibreOffice_{version}_Linux_x86-64_rpm.tar.gz"
 	artifacts = [hub.pkgtools.ebuild.Artifact(url=url)]
-
 	ebuild_bin = hub.pkgtools.ebuild.BreezyBuild(**pkginfo, version=version, artifacts=artifacts)
 	ebuild_bin.push()
 	await add_l10n_ebuild(hub, ebuild_bin, version=ebuild_bin.version, **pkginfo)

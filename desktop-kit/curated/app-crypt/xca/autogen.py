8e36ab66c016d3e4ce80c08d6674f5dac964ccbb
drobbins <drobbins@funtoo.org>
2022-02-07 19:45:51 +0000

    Merge pull request #1632 in CORE/kit-fixups from ~ROMIKB/kit-fixups:master to master
    Squashed commit of the following:
    
    commit c8faf193c7b3cba25cc743b3e7a6107092907839
    Author: romikb <romikb@mail.ru>
    Date:   Mon Feb 7 21:38:39 2022 +0200
    
        fix copyright and keywords
    
    commit fb88e91faaf16f4e7e21d4630601b0efb835b84e
    Author: romikb <romikb@mail.ru>
    Date:   Mon Feb 7 19:59:56 2022 +0200
    
        add xca autogen script based on gentoo ebuild

---
+++ desktop-kit/curated/app-crypt/xca/autogen.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from packaging import version
+
+
+def get_release(releases_data):
+	releases = list(filter(lambda x: x["prerelease"] is False and x["draft"] is False, releases_data))
+	return None if not releases else sorted(releases, key=lambda x: version.parse(x["tag_name"])).pop()
+
+
+async def generate(hub, **pkginfo):
+	github_user = "chris2511"
+	github_repo = pkginfo["name"]
+	json_list = await hub.pkgtools.fetch.get_page(
+		f"https://api.github.com/repos/{github_user}/{github_repo}/releases", is_json=True
+	)
+	latest_release = get_release(json_list)
+	if latest_release is None:
+		raise hub.pkgtools.ebuild.BreezyError(f"Can't find a suitable release of {github_repo}")
+	version = latest_release["tag_name"][8:]
+	dl_asset = list(filter(lambda x: "tar.gz" in x["name"], latest_release["assets"])).pop()
+	url = dl_asset["browser_download_url"]
+	artifact = hub.pkgtools.ebuild.Artifact(url=url)
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		version=version,
+		artifacts=[
+			artifact,
+		],
+	)
+	ebuild.push()

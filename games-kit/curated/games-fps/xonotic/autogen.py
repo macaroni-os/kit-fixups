2c3a07501d1fefcc3f8a6521fbce880d966c546a
drobbins <drobbins@funtoo.org>
2022-02-10 16:10:49 +0000

    Merge pull request #1640 in CORE/kit-fixups from FL-9426-xonotic-autogen to master
    Squashed commit of the following:
    
    commit deadd45beae85919b7c811cc4ba9d0bae17e61eb
    Author: invakid404 <invakid404@riseup.net>
    Date:   Thu Feb 10 09:32:54 2022 +0200
    
        Bump space requirements
    
    commit dead9cf628379675c57d9d92ea517d1305195373
    Author: invakid404 <invakid404@riseup.net>
    Date:   Thu Feb 10 09:30:16 2022 +0200
    
        Add an autogen for xonotic
    
    commit dead84ca2e9a9d3a1b3d034c4ab9fab10ab7efed
    Author: invakid404 <invakid404@riseup.net>
    Date:   Thu Feb 10 09:30:01 2022 +0200
    
        Fork xonotic

---
+++ games-kit/curated/games-fps/xonotic/autogen.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+from datetime import datetime
+import re
+
+
+async def generate(hub, **pkginfo):
+	autobuild_url = "https://beta.xonotic.org/autobuild"
+	src_pattern = re.compile("^\\.\\/(Xonotic-(\\d+)\.zip)$")
+
+	autobuild_soup = BeautifulSoup(
+		await hub.pkgtools.http.get_page(autobuild_url), "lxml"
+	)
+
+	link_matches = (
+		src_pattern.match(link.get("href")) for link in autobuild_soup.find_all("a")
+	)
+	valid_matches = (match.groups() for match in link_matches if match)
+
+	target_filename, target_version = max(
+		valid_matches,
+		key=lambda match: datetime.strptime(match[1], "%Y%m%d"),
+	)
+	src_url = f"{autobuild_url}/{target_filename}"
+
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		version=target_version,
+		artifacts=[hub.pkgtools.ebuild.Artifact(url=src_url)],
+	)
+	ebuild.push()
+

20a001f1d63ba33cc96b9cae5e59b7b879e4f4d0
drobbins <drobbins@funtoo.org>
2022-02-28 22:31:55 +0000

    Merge pull request #1681 in CORE/kit-fixups from FL-9478-github-1-repo-metadata to master
    Squashed commit of the following:
    
    commit deada71898358bfe821800e708af80e0b0cd6903
    Author: invakid404 <invakid404@riseup.net>
    Date:   Mon Feb 28 13:09:29 2022 +0200
    
        Use description in github-1-generated packages
    
    commit deadf7a1c27c4e42c2ce03e028f638680423ab61
    Author: invakid404 <invakid404@riseup.net>
    Date:   Mon Feb 28 10:31:43 2022 +0200
    
        Format github-1.py a bit
    
    commit deadd902f3bd290437363933680170bc9f839ad9
    Author: invakid404 <invakid404@riseup.net>
    Date:   Mon Feb 28 10:30:35 2022 +0200
    
        Fetch description via the GitHub API

--- generators/github-1.py
+++ generators/github-1.py
@@ -1,28 +1,52 @@
 #!/usr/bin/python3
 
+
 async def generate(hub, **pkginfo):
-	for key in ['user', 'repo']:
+	for key in ["user", "repo"]:
 		if key not in pkginfo:
-			if 'github' in pkginfo and key in pkginfo['github']:
-				pkginfo[f'github_{key}'] = pkginfo['github'][key]
+			if "github" in pkginfo and key in pkginfo["github"]:
+				pkginfo[f"github_{key}"] = pkginfo["github"][key]
 			else:
-				pkginfo[f'github_{key}'] = pkginfo['name']
-	query = pkginfo['github']['query']
+				pkginfo[f"github_{key}"] = pkginfo["name"]
+
+	query = pkginfo["github"]["query"]
 	if query not in ["releases", "tags"]:
-		raise KeyError(f"{pkginfo['cat']}/{pkginfo['name']} should specify GitHub query type of 'releases' or 'tags'.")
-	github_user = pkginfo['github_user']
-	github_repo = pkginfo['github_repo']
-	if 'homepage' not in pkginfo:
-		pkginfo['homepage'] = f"https://github.com/{github_user}/{github_repo}"
+		raise KeyError(
+			f"{pkginfo['cat']}/{pkginfo['name']} should specify GitHub query type of 'releases' or 'tags'."
+		)
+
+	github_user = pkginfo["github_user"]
+	github_repo = pkginfo["github_repo"]
+
+	if "homepage" not in pkginfo:
+		pkginfo["homepage"] = f"https://github.com/{github_user}/{github_repo}"
+
 	if query == "tags":
 		github_result = await hub.pkgtools.github.tag_gen(hub, github_user, github_repo)
 	else:
-		github_result = await hub.pkgtools.github.release_gen(hub, github_user, github_repo, tarball=pkginfo.get('tarball', None), select=pkginfo.get('select', None))
+		github_result = await hub.pkgtools.github.release_gen(
+			hub,
+			github_user,
+			github_repo,
+			tarball=pkginfo.get("tarball", None),
+			select=pkginfo.get("select", None),
+		)
+
 	if github_result is None:
-		raise KeyError(f"Unable to find suitable GitHub release/tag for {pkginfo['cat']}/{pkginfo['name']}.")
+		raise KeyError(
+			f"Unable to find suitable GitHub release/tag for {pkginfo['cat']}/{pkginfo['name']}."
+		)
 
 	pkginfo.update(github_result)
+
+	if "description" not in pkginfo:
+		repo_metadata = await hub.pkgtools.fetch.get_page(
+			f"https://api.github.com/repos/{github_user}/{github_repo}", is_json=True
+		)
+		pkginfo["description"] = repo_metadata["description"]
+
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(**pkginfo)
 	ebuild.push()
+
 
 # vim: ts=4 sw=4 noet

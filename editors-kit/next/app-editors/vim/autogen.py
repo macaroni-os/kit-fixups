6960b4500f0f88df4945599b71ffface9ff91112
alex2101 <alex2101@gmail.com>
2022-03-10 16:35:58 +0000

    Pull request #1720: FL-8679: Fix app-editors/vim autogen.
    Merge in CORE/kit-fixups from ~ALEX2101/kit-fixups:FL-8679-make-python3.9-the-default-in-next-release-5 to master
    
    Squashed commit of the following:
    
    commit f1cb8b62448b345ff94134fb9c953855be425c45
    Author: alex2101 <alex2101@gmail.com>
    Date:   Thu Mar 10 10:22:37 2022 -0300
    
        FL-8679: Fix app-editors/vim autogen.

---
+++ editors-kit/next/app-editors/vim/autogen.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import json
+from datetime import timedelta
+
+
+async def generate(hub, **pkginfo):
+	python_compat = "python2+"
+	patches = [
+		"002_all_vim-7.3-apache-83565.patch",
+		"004_all_vim-7.0-grub-splash-96155.patch",
+		"005_all_vim_7.1-ada-default-compiler.patch",
+		"006-vim-8.0-crosscompile.patch",
+	]
+	json_dict = await hub.pkgtools.fetch.get_page(
+		"https://api.github.com/repos/vim/vim/tags", refresh_interval=timedelta(days=7), is_json=True
+	)
+	release = json_dict[0]
+	version = release["name"][1:]  # strip 'v'
+	url = f"https://github.com/vim/vim/archive/v{version}/v{version}.tar.gz"
+	artifacts = [hub.pkgtools.ebuild.Artifact(url=url, final_name=f"{pkginfo['name']}-{version}.tar.gz")]
+	vim = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo, python_compat=python_compat, patches=patches, version=version, artifacts=artifacts
+	)
+	vim.push()
+	vim_core = hub.pkgtools.ebuild.BreezyBuild(
+		template_path=vim.template_path,
+		cat=pkginfo["cat"],
+		name="vim-core",
+		python_compat=python_compat,
+		patches=patches,
+		version=version,
+		artifacts=artifacts,
+	)
+	vim_core.push()
+	gvim = hub.pkgtools.ebuild.BreezyBuild(
+		template_path=vim.template_path,
+		cat=pkginfo["cat"],
+		name="gvim",
+		python_compat=python_compat,
+		patches=patches,
+		version=version,
+		artifacts=artifacts,
+	)
+	gvim.push()
+
+
+# vim: ts=4 sw=4 noet

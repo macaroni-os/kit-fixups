658727b40f3a2c516b50d417968944e125829a28
Daniel Robbins <drobbins@funtoo.org>
2022-03-13 23:23:28 -0600

    FL-8555: remove encoding= in get_page() of nextcloud autogen, not supported or needed with httpx HTTP backend.

--- net-kit/curated/www-apps/nextcloud/autogen.py
+++ net-kit/curated/www-apps/nextcloud/autogen.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 async def generate(hub, **pkginfo):
 	changelog_url = "https://nextcloud.com/changelog/"
-	changelog_data = await hub.pkgtools.fetch.get_page(changelog_url, encoding="utf-8")
+	changelog_data = await hub.pkgtools.fetch.get_page(changelog_url)
 	changelog_soup = BeautifulSoup(changelog_data, "html.parser")
 	latest_tags = [x for x in changelog_soup.find_all("a", attrs={"id": True}) if x["id"].startswith("latest")]
 	latest_urls = [x.find_next("a", href=True) for x in latest_tags]

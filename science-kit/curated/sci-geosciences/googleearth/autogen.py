aa42779fd50f26f4832494d7e622dd0fa6814f93
seemant <seemant@funtoo.org>
2022-03-16 20:57:19 +0000

    Pull request #1748: FL-9290: autogen Google Earth
    Merge in CORE/kit-fixups from ~SEEMANT/kit-fixups:FL-9290 to master
    
    Squashed commit of the following:
    
    commit 18f099b68f9a5805970cae1e66c9d5cd79edeedf
    Author: seemant <seemant@funtoo.org>
    Date:   Wed Mar 16 13:50:27 2022 -0700
    
        autogen googleearth
    
    commit e280193b4af49ee6efa13f770f3badbe6ef69f54
    Author: seemant <seemant@funtoo.org>
    Date:   Wed Mar 16 13:50:18 2022 -0700
    
        googleearth will be autogenned instead of synced from flora

---
+++ science-kit/curated/sci-geosciences/googleearth/autogen.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+from packaging.version import Version
+import re
+
+async def generate(hub, **pkginfo):
+    releases_page = "https://support.google.com/earth/answer/168344"
+    regex = r'(\d+(?:\.\d+)+)'
+
+    html = await hub.pkgtools.fetch.get_page(releases_page)
+    soup = BeautifulSoup(html, features="html.parser").find_all("a")
+
+    debs = [a.get('href') for a in soup if a.has_attr('href') and a.get('href').endswith('deb')]
+    latest_release = max([(Version(re.findall(regex, a)[0]), a) for a in debs])
+
+    ebuild = hub.pkgtools.ebuild.BreezyBuild(
+        **pkginfo,
+        version=latest_release[0],
+        artifacts=[hub.pkgtools.ebuild.Artifact(url=latest_release[1])],
+    )
+    ebuild.push()
+
+
+
+#vim: ts=4 sw=4 noet

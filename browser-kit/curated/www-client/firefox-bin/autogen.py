7d5f9069f192ea942a88854b7dd64569ce781223
siris <ssofos@gmail.com>
2022-03-20 14:24:23 +0000

    Pull request #1764: FL-9572: add new firefox-dev-bin autogen to existing firefox-bin autogen
    Merge in CORE/kit-fixups from ~SIRIS/kit-fixups:FL-9572/add-firefox-dev-bin to master
    
    Squashed commit of the following:
    
    commit e291c72175d7bedaa57016a08b4fd9527c70d847
    Author: Shane R. Sofos <ssofos@gmail.com>
    Date:   Sun Mar 20 00:27:09 2022 -0700
    
        FL-9572: add new firefox-dev-bin autogen to existing firefox-bin autogen

--- browser-kit/curated/www-client/firefox-bin/autogen.py
+++ browser-kit/curated/www-client/firefox-bin/autogen.py
@@ -21,13 +21,13 @@ async def get_lang_artifacts(hub, version):
 	return dict(artifacts=artifacts, lang_codes=lang_codes)
 
 
-def get_artifact(hub, version, arch):
+def get_artifact(hub, name, version, arch):
 	if arch == "amd64":
 		moz_arch = "x86_64"
 	elif arch == "x86":
 		moz_arch = "i686"
 	url = f"https://archive.mozilla.org/pub/firefox/releases/{version}/linux-{moz_arch}/en-US/firefox-{version}.tar.bz2"
-	final_name = f"firefox-bin_{moz_arch}-{version}.tar.bz2"
+	final_name = f"{name}_{moz_arch}-{version.replace('b','_beta')}.tar.bz2"
 	return hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)
 
 
@@ -35,14 +35,27 @@ async def generate(hub, **pkginfo):
 	json_data = await hub.pkgtools.fetch.get_page("https://product-details.mozilla.org/1.0/firefox_versions.json")
 	json_dict = json.loads(json_data)
 	version = json_dict["LATEST_FIREFOX_VERSION"]
+	version_dev = json_dict["LATEST_FIREFOX_RELEASED_DEVEL_VERSION"]
+	version_dev_final = version_dev.replace('b','_beta')
 	lang_data = await get_lang_artifacts(hub, version)
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
+		install_dir="/opt/firefox",
 		version=version,
 		lang_codes=" ".join(sorted(lang_data["lang_codes"])),
-		artifacts=[get_artifact(hub, version, "amd64"), get_artifact(hub, version, "x86"), *lang_data["artifacts"]],
+		artifacts=[get_artifact(hub, pkginfo['name'], version, "amd64"), get_artifact(hub, pkginfo['name'], version, "x86"), *lang_data["artifacts"]],
 	)
 	ebuild.push()
+	ebuild_dev = hub.pkgtools.ebuild.BreezyBuild(
+		template_path=ebuild.template_path,
+		template=ebuild.template,
+		cat=pkginfo["cat"],
+		name="firefox-dev-bin",
+		install_dir="/opt/firefox-dev",
+		version=version_dev_final,
+		artifacts=[get_artifact(hub, "firefox-dev-bin", version_dev, "amd64"), get_artifact(hub, "firefox-dev-bin", version_dev, "x86"), *lang_data["artifacts"]],
+	)
+	ebuild_dev.push()
 
 
 # vim: ts=4 sw=4 noet

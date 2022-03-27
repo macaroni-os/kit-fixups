9e3c73d958da643e7f4a4ab44bed6433103facd0
Daniel Robbins <drobbins@funtoo.org>
2022-03-26 22:14:45 -0600

    Vivaldi has changed their deb structure to have separate dirs for each kind of release. This adapts to this new model and cleans things up to be easier for me to troubleshoot.

--- browser-kit/curated/www-client/vivaldi/autogen.py
+++ browser-kit/curated/www-client/vivaldi/autogen.py
@@ -2,6 +2,8 @@
 
 from bs4 import BeautifulSoup
 from packaging.version import Version
+from collections import defaultdict
+from datetime import timedelta
 import re
 
 arches = {
@@ -10,17 +12,23 @@ arches = {
 	'arm64': 'arm64',
 }
 
-url = "https://repo.vivaldi.com/archive/deb/pool/main/"
-
-def generate_ebuild(key, debs, **pkginfo):
-	releases = [a for a in debs if key in a]
-	latest = max([Version(a.split('_')[1]) for a in releases])
-	version = f"{latest.base_version}_p{latest.post}"
-
-	artifacts = dict([
-		(arches[(a.split('_')[2].split('.')[0])], hub.pkgtools.ebuild.Artifact(url=url + a))
-	for a in releases])
-
+def generate_ebuild(base_url, key, debs, **pkginfo):
+	candidates = defaultdict(lambda: dict())
+	for deb in debs:
+		found = re.match(f"vivaldi-{key}_([0-9.-]+)_(.*)\.deb", deb)
+		if not found:
+			continue
+		else:
+			groups = found.groups()
+			candidates[groups[0]][groups[1]] = deb
+	if not len(candidates):
+		return
+	latest = sorted(candidates.keys(), key=lambda x: Version(x.replace("-", ".")), reverse=True)[0]
+	candidate = candidates[latest]
+	artifacts = {}
+	for arch, url in candidate.items():
+		artifacts[arch] = hub.pkgtools.ebuild.Artifact(url=base_url+url)
+	version = latest.replace("-", "_p")
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
 		version=version,
@@ -31,15 +39,14 @@ def generate_ebuild(key, debs, **pkginfo):
 
 
 async def generate(hub, **pkginfo):
-	html = await hub.pkgtools.fetch.get_page(url)
-	soup = BeautifulSoup(html, features="html.parser").find_all("a")
-
-	debs = [a.contents[0] for a in soup if a.contents[0].endswith('deb')]
-	generate_ebuild("stable", debs, **pkginfo)
-
-	pkginfo['name'] = 'vivaldi-snapshot'
-	generate_ebuild("snapshot", debs, **pkginfo)
-
-
+	base_url = "https://repo.vivaldi.com/{release}/deb/pool/main/"
+	for release in [ 'stable', 'snapshot' ]:
+		real_url = base_url.format(release=release)
+		html = await hub.pkgtools.fetch.get_page(real_url, refresh_interval=timedelta(days=3))
+		soup = BeautifulSoup(html, features="html.parser").find_all("a")
+		debs = [a.contents[0] for a in soup if a.contents[0].endswith('deb')]
+		if release != "stable":
+			pkginfo['name'] = f"vivaldi-{release}"
+		generate_ebuild(real_url, release, debs, **pkginfo)
 
 # vim: ts=4 sw=4 noet

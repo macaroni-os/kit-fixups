66ca49ceef5dd54930edaa98fede83b28d974f68
seemant <seemant@funtoo.org>
2022-04-03 16:40:06 +0000

    Pull request #1818: FL-9619: autogen elasticsearch along with kibana-bin
    Merge in CORE/kit-fixups from ~SEEMANT/kit-fixups:FL-9619 to master
    
    Squashed commit of the following:
    
    commit 49618f59e18bef844d3ea3aee196dc0bf972dc47
    Merge: 4f3c32823 0cf481332
    Author: seemant <seemant@funtoo.org>
    Date:   Sun Apr 3 01:54:13 2022 -0700
    
        Merge branch 'master' into FL-9619
    
    commit 4f3c32823f06cf1d35f16a3fdcf27b28c860bead
    Author: seemant <seemant@funtoo.org>
    Date:   Sun Apr 3 01:53:08 2022 -0700
    
        add filesdir for elasticsearch
    
    commit ec3b2b2c202718e700bcc2dadb6bb5576422c514
    Author: seemant <seemant@funtoo.org>
    Date:   Sun Apr 3 01:51:38 2022 -0700
    
        remove typo
    
    commit b462a5b414e322db5d1761899dacf4a7b3dfa1ae
    Author: seemant <seemant@funtoo.org>
    Date:   Sun Apr 3 01:50:55 2022 -0700
    
        simplify the src_uri logic and add missing quoteation mark
    
    commit 98ffba1cbd9f7dcbf9ed12e71ca0335f34babe27
    Author: seemant <seemant@funtoo.org>
    Date:   Sun Apr 3 01:18:37 2022 -0700
    
        add template for elasticsearch, based on gentoo's latest.  this is a combined template just like kibana-bin
    
    commit 8697192f81856c0cf216a535bbab1e22183177e4
    Author: seemant <seemant@funtoo.org>
    Date:   Sun Apr 3 01:18:03 2022 -0700
    
        generate package for app-misc/elasticsearch as well
    
    commit 4af05f2bb26a25d2f553811181365b24cad3231a
    Author: seemant <seemant@funtoo.org>
    Date:   Sun Apr 3 01:16:03 2022 -0700
    
        use package version rather than node version to add/remove statements
    
    commit 311d4f5d94d6d958fb14d6eb3665741dfc6798a8
    Author: seemant <seemant@funtoo.org>
    Date:   Sat Apr 2 23:09:40 2022 -0700
    
        move the autogen into a neutral directory, rather than a specific package name, as it will autogenerate a number of pacakges
    
    commit d9fcb77a311020063a216223e22721761d0fedac
    Author: seemant <seemant@funtoo.org>
    Date:   Sat Apr 2 21:32:53 2022 -0700
    
        dynamically get the 3 latest major versions
    
    commit 335f9b1807deed2ecd3eef702481a240a10c7b3f
    Author: seemant <seemant@funtoo.org>
    Date:   Sat Apr 2 21:06:43 2022 -0700
    
        unified template, v6 is nodejs 12, and v7/8 are nodejs16, so use that information to construct the v6 bits
    
    commit 2d8e31dce6ae6e6ecc5b616598c862cd2f585106
    Author: seemant <seemant@funtoo.org>
    Date:   Sat Apr 2 21:05:35 2022 -0700
    
        add kibana initscript for v6
    
    commit f037ee7e3ec43992fb2006838a3f58332d4973f0
    Author: seemant <seemant@funtoo.org>
    Date:   Sat Apr 2 21:05:17 2022 -0700
    
        find the minimum version of nodejs and major version of valid nodejs. use single template for all versions
    
    commit f5cc4c1ca091f0a1a3a261889cbbc6e5a141321c
    Author: seemant <seemant@funtoo.org>
    Date:   Sat Apr 2 21:04:00 2022 -0700
    
        avoid version specific templates
    
    commit d24c00815d3bb108eac7dab130f0fa521dfc7524
    Author: seemant <seemant@funtoo.org>
    Date:   Sat Apr 2 16:37:48 2022 -0700
    
        add autogen for kibana v7 and v8, template based on Gentoo's v7 template
    
    commit a74a25fd634eeae160ab8b5d38f7e1dffae8c0d3
    Author: seemant <seemant@funtoo.org>
    Date:   Sat Apr 2 16:35:50 2022 -0700
    
        kibana-bin will be autogenned, not sync'd

---
+++ core-server-kit/curated/www-apps/elastic/autogen.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+from packaging.version import Version
+import json
+import re
+import os
+
+
+MINIMUM_STABLE_NODEJS = 12
+
+def portage_arch(arch):
+	arches = { # map upstream arch string to portage arch string
+		'x86_64': 'amd64',
+		'aarch64': 'arm64'
+	}
+	return arches.get(arch) or 'amd64'
+
+
+async def generate_artifacts(hub, pkginfo, urls):
+	return [(
+			portage_arch(url.split('-')[-1].split('.')[0]), # portage arch string
+			hub.pkgtools.ebuild.Artifact(url=url)
+		) for url in urls if 'sha' not in url # filter out the sha
+	]
+
+
+async def get_minimum_node_version(artifact):
+	# extract the contents of package.json in the src tarball
+	await artifact.fetch()
+	artifact.extract()
+	try: # versions 6, 7
+		package = os.path.join(artifact.extract_path, artifact.final_name.split(".tar.")[0], "package.json")
+		package_info = json.load(open(package))
+	except FileNotFoundError: # version 8
+		package = os.path.join(artifact.extract_path, artifact.final_name.split("-linux")[0], "package.json")
+		package_info = json.load(open(package))
+	artifact.cleanup()
+
+	version = Version(package_info["engines"]["node"])
+	return { 'minimum': version.public, 'series': max(version.major, MINIMUM_STABLE_NODEJS) }
+
+
+async def generate(hub, **pkginfo):
+	download_url = "https://www.elastic.co/downloads/past-releases"
+	github_user = "elastic"
+	packages = {
+		"www-apps/kibana-bin": "kibana",
+		"app-misc/elasticsearch": "elasticsearch",
+	}
+
+	for package in packages:
+		github_repo = packages[package]
+		releases = await hub.pkgtools.fetch.get_page(
+			f"https://api.github.com/repos/{github_user}/{github_repo}/releases",
+			is_json=True
+		)
+		versions = [Version(a["tag_name"].lstrip("v")) for a in releases]
+		latest = max([v for v in versions])
+
+		# Create an ebuild for the most recent 3 major versions
+		for major in [latest.major, latest.major - 1, latest.major - 2]:
+			version = max([v for v in versions if v.major == major])
+			pkginfo["cat"], pkginfo["name"] = package.split("/")
+			name = pkginfo["name"].replace("-bin","")
+
+			# find available architecture tarballs
+			download_page = f"{download_url}/{name}-{version.public.replace('.', '-')}"
+			html = await hub.pkgtools.fetch.get_page(download_page)
+			soup = BeautifulSoup(html, "html.parser").find_all("a", href=True)
+			tarballs = [a.get('href') for a in soup if 'linux' in a.get('href')]
+			if len(tarballs) == 0:
+				tarballs = [a.get('href') for a in soup if '.tar.' in a.get('href')]
+
+			print(tarballs)
+
+			artifacts = await generate_artifacts(hub, pkginfo, tarballs)
+
+			# find the compatible node version for kibana-bin
+			if "kibana" in name:
+				pkginfo['nodejs'] = await get_minimum_node_version(artifacts[0][1])
+
+			template = f"-{major}"
+			if major != 6:
+				template = "-7"
+
+			#if "search" in pkginfo["name"]:
+			#	pkginfo["template"] = pkginfo["name"] + template + ".tmpl"
+
+			ebuild = hub.pkgtools.ebuild.BreezyBuild(
+				**pkginfo,
+				version=version.public.replace('-', '_'),
+				major=major,
+				artifacts=dict(artifacts),
+			)
+			ebuild.push()
+
+# vim: ts=4 sw=4 noet

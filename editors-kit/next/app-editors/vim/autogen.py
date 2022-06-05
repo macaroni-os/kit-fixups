4140cfa0093ba89744022959c1c8ebfc07126e31
Daniel Robbins <drobbins@funtoo.org>
2022-06-05 17:50:04 -0600

    Hotfix for vim -- remove fragile (and likely unnecessary) patches.

--- editors-kit/next/app-editors/vim/autogen.py
+++ editors-kit/next/app-editors/vim/autogen.py
@@ -6,12 +6,6 @@ from datetime import timedelta
 
 async def generate(hub, **pkginfo):
 	python_compat = "python2+"
-	patches = [
-		"002_all_vim-7.3-apache-83565.patch",
-		"004_all_vim-7.0-grub-splash-96155.patch",
-		"005_all_vim_7.1-ada-default-compiler.patch",
-		"006-vim-8.0-crosscompile.patch",
-	]
 	json_dict = await hub.pkgtools.fetch.get_page(
 		"https://api.github.com/repos/vim/vim/tags", refresh_interval=timedelta(days=7), is_json=True
 	)
@@ -20,7 +14,7 @@ async def generate(hub, **pkginfo):
 	url = f"https://github.com/vim/vim/archive/v{version}/v{version}.tar.gz"
 	artifacts = [hub.pkgtools.ebuild.Artifact(url=url, final_name=f"{pkginfo['name']}-{version}.tar.gz")]
 	vim = hub.pkgtools.ebuild.BreezyBuild(
-		**pkginfo, python_compat=python_compat, patches=patches, version=version, artifacts=artifacts
+		**pkginfo, python_compat=python_compat, version=version, artifacts=artifacts
 	)
 	vim.push()
 	vim_core = hub.pkgtools.ebuild.BreezyBuild(
@@ -28,7 +22,6 @@ async def generate(hub, **pkginfo):
 		cat=pkginfo["cat"],
 		name="vim-core",
 		python_compat=python_compat,
-		patches=patches,
 		version=version,
 		artifacts=artifacts,
 	)
@@ -38,7 +31,6 @@ async def generate(hub, **pkginfo):
 		cat=pkginfo["cat"],
 		name="gvim",
 		python_compat=python_compat,
-		patches=patches,
 		version=version,
 		artifacts=artifacts,
 	)

310bb36e6d521e4cf556d6981ad9a4021f452e59
Daniel Robbins <drobbins@funtoo.org>
2022-07-27 14:41:41 -0600

    Continued work on fixing compatibility in github-1 generator.

--- generators/github-1.py
+++ generators/github-1.py
@@ -2,12 +2,25 @@
 
 
 async def generate(hub, **pkginfo):
+	# migrate keys inside "github:" element to "github_foo":
+	if "github" not in pkginfo:
+		pkginfo["github"] = {}
 	for key in ["user", "repo"]:
 		if f"github_{key}" not in pkginfo:
 			if "github" in pkginfo and key in pkginfo["github"]:
 				pkginfo[f"github_{key}"] = pkginfo["github"][key]
 			else:
 				pkginfo[f"github_{key}"] = pkginfo["name"]
+
+	# promote "match" and "select" in pkginfo into github element, so we support both inside and outside
+	# this element for these keys:
+
+	for key in ["match", "select"]:
+		if key in pkginfo:
+			if "github" in pkginfo and key in pkginfo["github"]:
+				raise ValueError(f"{key} defined in both main YAML block and github block -- chose one.")
+			pkginfo["github"][key] = pkginfo[key]
+			del pkginfo[key]
 
 	query = pkginfo["github"]["query"]
 	if query not in ["releases", "tags"]:
@@ -22,15 +35,25 @@ async def generate(hub, **pkginfo):
 		pkginfo["homepage"] = f"https://github.com/{github_user}/{github_repo}"
 
 	extra_args = {}
-	if "select" in pkginfo and "match" in pkginfo:
-		raise valueError("Please use either 'select' or 'match' but not both.")
-	for arg in ["version", "select"]:
+	if "select" in pkginfo and "match" in pkginfo["github"]:
+		raise ValueError("Please use either 'select' or 'match' but not both.")
+
+	# Extra args handling:
+
+	for arg in ["version"]:
 		extra_args[arg] = pkginfo[arg] if arg in pkginfo else None
 	if extra_args["version"] == "latest":
 		del extra_args["version"]
-	if "match" in pkginfo:
+
+	# GitHub args handling:
+
+	for gh_arg in ["select"]:
+		if gh_arg in pkginfo["github"]:
+			extra_args[gh_arg] = pkginfo["github"][gh_arg]
+
+	if "match" in pkginfo["github"]:
 		# explicit match from YAML:
-		extra_args["matcher"] = hub.pkgtools.github.RegexMatcher(regex=pkginfo["match"])
+		extra_args["matcher"] = hub.pkgtools.github.RegexMatcher(regex=pkginfo["github"]["match"])
 	elif "select" in extra_args:
 		# If a user specifies "select", they probably want the classic grabby matcher and are using "select" to filter undesireables:
 		extra_args["matcher"] = hub.pkgtools.github.RegexMatcher(regex=hub.pkgtools.github.VersionMatch.GRABBY)
@@ -70,6 +93,5 @@ async def generate(hub, **pkginfo):
 		pkginfo["description"] = repo_metadata["description"]
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(**pkginfo)
 	ebuild.push()
-
 
 # vim: ts=4 sw=4 noet

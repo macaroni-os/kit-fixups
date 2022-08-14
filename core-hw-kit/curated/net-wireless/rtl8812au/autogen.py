568365b192650976a032fed8d262d180ee3ecd94
madman10k <contact@madman10k.online>
2022-08-14 11:16:49 +0000

    Pull request #2242: FL-10299: Add ebuild for rtl8812au driver
    Merge in CORE/kit-fixups from ~MADMAN10K/kit-fixups:FL-10299-add-ebuild-for-rtl8812au-driver to master
    
    Squashed commit of the following:
    
    commit a4dee88d3d2bf7b527c21ed8899bbd498e04fc36
    Author: Madman10K <stanv.contact176@gmail.com>
    Date:   Mon Aug 8 20:11:17 2022 +0300
    
        Add rtl8812au driver autogen

---
+++ core-hw-kit/curated/net-wireless/rtl8812au/autogen.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from datetime import datetime, timedelta
+
+async def query_github_api(user, repo, query):
+    return await hub.pkgtools.fetch.get_page(
+        f"https://api.github.com/repos/{user}/{repo}/{query}",
+        is_json=True,
+        refresh_interval=timedelta(days=15)
+    )
+
+async def is_valid(user, repo, commit):
+    commit_sha = commit["sha"]
+    commit_status = await query_github_api(user, repo, f"commits/{commit_sha}/status")
+
+    return "failure" not in commit_status["state"]
+
+async def generate(hub, **pkginfo):
+    github_user = "aircrack-ng"
+    github_repo = "rtl8812au"
+
+    commits = await query_github_api(github_user, github_repo, "commits?sha=v5.6.4.2")
+
+    valid_commits = (commit for commit in commits if await is_valid(github_user, github_repo, commit))
+
+    target_commit = await valid_commits.__anext__()
+
+    commit_date = datetime.strptime(target_commit["commit"]["committer"]["date"], "%Y-%m-%dT%H:%M:%SZ")
+
+    commit_hash = target_commit["sha"]
+    version = commit_date.strftime("%Y%m%d")
+    url = f"https://github.com/{github_user}/{github_repo}/archive/{commit_hash}.tar.gz"
+    final_name = f"{pkginfo['name']}-{version}.tar.gz"
+
+    ebuild = hub.pkgtools.ebuild.BreezyBuild(
+        **pkginfo,
+        github_user=github_user,
+        github_repo=github_repo,
+        version=version,
+        artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)],
+    )
+
+    ebuild.push()

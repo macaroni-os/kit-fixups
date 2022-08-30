bf963a7115f673b7e4fdc203beaab9687264c822
siris <ssofos@gmail.com>
2022-08-30 15:47:55 +0000

    Pull request #2300: FL-10392: Updating Project Telemetry bin autogens to support arm64 arch
    Merge in CORE/kit-fixups from ~SIRIS/kit-fixups:FL-10392/project-telemetry-arm64 to master
    
    Squashed commit of the following:
    
    commit b1f161a4f1f872953b637d0f4a65221a27b60a6f
    Author: Shane R. Sofos <ssofos@gmail.com>
    Date:   Sun Aug 28 15:16:43 2022 -0700
    
        FL-10392: Updating Project Telemetry bin autogens to support arm64 arch

--- net-kit/curated/www-apps/grafana-bin/autogen.py
+++ net-kit/curated/www-apps/grafana-bin/autogen.py
@@ -13,9 +13,8 @@ def find_release(json_dict):
 		return None
 	return sorted(releases, key=lambda x: version.parse(x["tag_name"]))[-1]
 
-def find_release_tarball(release_version):
+def find_release_tarball(release_version, release_arch):
 	upstream_url = "https://dl.grafana.com/oss/release/grafana-"
-	release_arch = "linux-amd64"
 	tarball_url = upstream_url + str(release_version) + "." + release_arch + ".tar.gz"
 	return tarball_url
 
@@ -26,11 +25,12 @@ async def generate(hub, **pkginfo):
 	if release is None:
 		raise hub.pkgtools.ebuild.BreezyError("Can't find a suitable release of Grafana.")
 	version = release["tag_name"][1:]
-	src_artifact = hub.pkgtools.ebuild.Artifact(url=find_release_tarball(version))
+	src_artifact_amd64 = hub.pkgtools.ebuild.Artifact(url=find_release_tarball(version, "linux-amd64"))
+	src_artifact_arm64 = hub.pkgtools.ebuild.Artifact(url=find_release_tarball(version, "linux-arm64"))
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
 		version=version,
-		artifacts=[src_artifact]
+		artifacts=[src_artifact_amd64, src_artifact_arm64]
 	)
 	ebuild.push()
 

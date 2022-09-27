6f04b77dcf8942ab267445105158a1f41fec73ba
Daniel Robbins <drobbins@funtoo.org>
2022-09-26 19:49:19 -0600

    FL-10538: disable external readline for bash by default. Also add an automated test for dynamic archives.

--- funtoo-test/dynamic-archive/autogen.py
+++ funtoo-test/dynamic-archive/autogen.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import os
+import asyncio
 
 """
 This test attempts to create a dynamic archive, which is a locally-created archive that will exist
@@ -21,25 +22,25 @@ We would expect both archives to be created, and for the last two debug statemen
 async def generate(hub, **pkginfo):
     id_key = "abcde"
     pkginfo["version"] = "1.0"
-    my_archive = hub.Archive.find(f"dynamic-archive-1.0-{id_key}.tar.xz", key={"id_key": id_key})
+    my_archive, metadata = hub.Archive.find_by_name(f"dynamic-archive-1.0-{id_key}.tar.xz", key={"id_key": id_key})
     if not my_archive:
         my_archive = hub.Archive(f"dynamic-archive-1.0-{id_key}.tar.xz")
         my_archive.initialize("dynamic-archive-1.0")
         with open(os.path.join(my_archive.top_path, "README"), "w") as myf:
             myf.write("HELLO")
-        my_archive.store(key={"id_key" : id_key})
+        my_archive.store_by_name(key={"id_key" : id_key})
     my_key_archive = my_archive
 
-    my_archive = hub.Archive.find("dynamic-archive-1.0.tar.xz")
+    my_archive, metadata = hub.Archive.find_by_name("dynamic-archive-1.0.tar.xz")
     if not my_archive:
         my_archive = hub.Archive("dynamic-archive-1.0.tar.xz")
         my_archive.initialize("dynamic-archive-1.0")
         with open(os.path.join(my_archive.top_path, "README"), "w") as myf:
             myf.write("HELLO")
-        my_archive.store()
+        my_archive.store_by_name()
     eb = hub.pkgtools.ebuild.BreezyBuild(**pkginfo, artifacts=[my_key_archive, my_archive])
     eb.push()
 
     hub.pkgtools.model.log.debug(f"    Keyed archive BLOS path: {my_key_archive.blos_object.blob.path}")
     hub.pkgtools.model.log.debug(f"Non-keyed archive BLOS path: {my_archive.blos_object.blob.path}")
-
+    #import pdb; pdb.set_trace()

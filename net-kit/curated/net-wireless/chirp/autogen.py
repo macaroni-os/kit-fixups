6d0819d337128bb27e1a9893ac260bc387ecd2af
adbosco <adbosco@funtoo.org>
2023-03-24 16:28:04 +0000

    Pull request #2760: FL-11022: Upgraded net-wireless chirp from "legacy" to "next".
    Merge in CORE/kit-fixups from ~ADBOSCO/kit-fixups:FL-11022 to master
    
    Squashed commit of the following:
    
    commit 3a71decb3fc2bdf48f90aa410f978170b60cc3fc
    Author: Adriano Dal Bosco <adbosco@funtoo.org>
    Date:   Wed Mar 22 21:51:28 2023 +0000
    
        Moved dev-python yattag from next/ to curated/.
    
    commit 9501619db0f5d861db938960dd7718c6fa1cbf8a
    Author: Adriano Dal Bosco <adbosco@funtoo.org>
    Date:   Wed Mar 22 01:48:40 2023 +0000
    
        Upgraded net-wireless/chirp autogen
    
        The existing autogen was getting the sources from "chip legacy". The new
        autogen uses "chirp next" and gets the latest updates.
    
        The "next" version is completely different from the "legacy" one, so not
        only the autogen generator, but also the template underwent substantial
        changes.
    
    commit 343e29110c15294663f486edea98a34ec62fcbd5
    Author: Adriano Dal Bosco <adbosco@funtoo.org>
    Date:   Wed Mar 22 01:37:19 2023 +0000
    
        Added dev-python/yattag
    
        This is a dependency for net-wireless/chirp.

--- net-kit/curated/net-wireless/chirp/autogen.py
+++ net-kit/curated/net-wireless/chirp/autogen.py
@@ -5,12 +5,25 @@ import re
 
 async def generate(hub, **pkginfo):
 
-	html = await hub.pkgtools.fetch.get_page("https://trac.chirp.danplanet.com/chirp_daily/LATEST")
-	version = (re.search(f"chirp-legacy-([0-9]*).tar.gz", html)).group(1)
-	url = f"https://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-legacy-{version}.tar.gz"
-	artifacts = [hub.pkgtools.ebuild.Artifact(url=url)]
+	pkginfo["homepage"] = "https://chirp.danplanet.com/ http://github.com/kk7ds/chirp"
+	pkginfo["description"] = "A free, open-source tool for programming your radio."
+	pkginfo["python_compat"] = "python3+" # As of v.20230319 needs to by <=3.10.
 
-	ebuild = hub.pkgtools.ebuild.BreezyBuild(**pkginfo, version=version, python_compat="python2_7", artifacts=artifacts)
+	dnl_page = await hub.pkgtools.fetch.get_url_from_redirect("https://trac.chirp.danplanet.com/download?stream=next")
+	hub.pkgtools.model.log.debug(f"Download page URL: {dnl_page}")
+
+	match = re.search("\d{8}$", dnl_page)
+	if not match:
+		raise KeyError("Could not find a suitable version.")
+	
+	pkginfo["version"] = match[0]
+
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(**pkginfo, 
+		artifacts=[hub.pkgtools.ebuild.Artifact(
+			url=f'https://trac.chirp.danplanet.com/chirp_next/next-{pkginfo["version"]}/chirp-{pkginfo["version"]}.tar.gz'
+			)
+		]
+	)
 	ebuild.push()
 
 

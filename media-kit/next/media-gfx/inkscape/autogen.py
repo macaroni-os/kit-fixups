af403a77d4e83cbef8bab6718d039d9532710bf9
seemant <seemant@funtoo.org>
2023-04-01 23:06:12 +0000

    Pull request #2781: FL-11150: only autogen stable versions of inkscape
    Merge in CORE/kit-fixups from ~SEEMANT/kit-fixups:FL-11150 to master
    
    Squashed commit of the following:
    
    commit 9afceebcdaf2d8fccfef312d0269d81b83bc33f7
    Author: seemant <seemant@funtoo.org>
    Date:   Fri Mar 31 22:12:43 2023 -0700
    
        enhance the regex to take into account prerelease suffixes, and discard prerelease versions from the max version calculation

--- media-kit/next/media-gfx/inkscape/autogen.py
+++ media-kit/next/media-gfx/inkscape/autogen.py
@@ -5,7 +5,7 @@ from packaging.version import Version
 import os
 import re
 
-regex = r'(\d+(?:\.\d+)+)'
+regex = r'(\d+(?:\.\d+)+[a-z]*(?:\d*))'
 
 
 async def generate(hub, **pkginfo):
@@ -16,7 +16,7 @@ async def generate(hub, **pkginfo):
 
     tarballs = [a.get('href') for a in soup if '.tar.' in a.contents[0] and not a.contents[0].endswith('sig')]
     versions = [(Version(re.findall(regex, a)[0]), a) for a in tarballs if re.findall(regex, a)]
-    latest = max(versions)
+    latest = max([v for v in versions if not v[0].is_prerelease])
 
     artifact = hub.pkgtools.ebuild.Artifact(url=base_url+latest[1])
 

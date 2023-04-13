702ea7cb5bb327818a8f64ba3c039ace9274472c
adbosco <adbosco@funtoo.org>
2023-04-13 14:20:06 +0000

    Pull request #2839: FL-11015: Updated net-p2p/transmission autogen.
    Merge in CORE/kit-fixups from ~ADBOSCO/kit-fixups:FL-11015 to master
    
    Squashed commit of the following:
    
    commit d4b97481d9c67f78c94984153bbdc589845b757d
    Author: Adriano Dal Bosco <adbosco@funtoo.org>
    Date:   Thu Apr 13 04:06:26 2023 +0000
    
        Updated net-p2p/transmission autogen.
    
        Version 4.0.0 was autogenerated, with important changes, including the
        replacement of "raw" C GTK+3 with C++ Gtkmm for the GTK GUI, which
        required some maintenance to the autogen.
    
        This new versions has some extra dependencies, which would mean the
        addition of some extra ebuilds to fulfil the requirements.  However, all
        these depedencies are actually vendored through git submodules when the
        sources are cloned from therepository, but they don't come with the
        release tarball.
    
        For this reason, I changed the autogen to use dynamic_archives, so that
        the sources can be cloned with the dependencies from the git repository
        when a new ebuild is autogenerated.  This allowed me not to just avoid
        the new depedencies, but I was also able to drop some dependencies it
        had before.

--- net-kit/curated/net-p2p/transmission/autogen.py
+++ net-kit/curated/net-p2p/transmission/autogen.py
@@ -1,40 +1,44 @@
 #!/usr/bin/env python3
 
-import json
+import os
 from packaging import version
 
 
+def get_release(releases_data):
+	releases = list(filter(lambda x: x["prerelease"] is False and x["draft"] is False, releases_data))
+	return None if not releases else sorted(releases, key=lambda x: version.parse(x["tag_name"])).pop()
+
+
 async def generate(hub, **pkginfo):
-	github_user = "transmission"
-	github_repo = "transmission"
-	app = pkginfo["name"]
+	github_user = pkginfo["name"]
+	github_repo = pkginfo["name"]
+
 	json_list = await hub.pkgtools.fetch.get_page(
-		f"https://api.github.com/repos/{github_user}/{github_repo}/releases",
-		is_json=True,
+		f"https://api.github.com/repos/{github_user}/{github_repo}/releases", is_json=True
 	)
+	latest_release = get_release(json_list)
+	if latest_release is None:
+		raise hub.pkgtools.ebuild.BreezyError(f"Can't find a suitable release of {github_repo}")
+	version = latest_release["tag_name"]
 
-	json_list = sorted(json_list, key=lambda x: version.parse(x["tag_name"]), reverse=True)
+	final_name = f'{pkginfo["name"]}-{version}-with-submodules.tar.xz'
 
-	for release in json_list:
-		if release["prerelease"] or release["draft"]:
-			continue
-		tag_name = release["tag_name"][0:]
-		if tag_name == "3.00":
-			break
+	my_archive = hub.Archive.find(final_name=final_name)
+	if my_archive is None:
+		my_archive = hub.Archive(final_name)
+		my_archive.initialize()
+		retval = os.system(f'( cd {my_archive.top_path}; git clone --depth 1 --branch {version} --recursive https://github.com/{github_user}/{github_repo}.git {pkginfo["name"]}-{version} )')
+		if retval != 0:
+			raise hub.pkgtools.ebuild.BreezyError("Unable to git clone repository.")
+		my_archive.store()
 
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
-		version=tag_name,
+		version=version,
 		github_user=github_user,
 		github_repo=github_repo,
-		artifacts=[
-			hub.pkgtools.ebuild.Artifact(
-				url=f"https://github.com/transmission/transmission-releases/raw/master/{app}-{tag_name}.tar.xz",
-				final_name=f"{app}-{tag_name}.tar.xz",
-			)
-		],
+		artifacts=[my_archive]
 	)
 	ebuild.push()
-
 
 # vim: ts=4 sw=4 noet

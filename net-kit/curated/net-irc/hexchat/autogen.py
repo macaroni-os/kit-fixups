1f887914720ad598ec5164b28fefa4b872dc5720
adbosco <adbosco@funtoo.org>
2023-04-16 01:01:03 +0000

    Pull request #2857: FL-9728: Autogenned net-irc/hexchat
    Merge in CORE/kit-fixups from ~ADBOSCO/kit-fixups:FL-9728 to master
    
    Squashed commit of the following:
    
    commit a47774d9fa716f863e0785040d4526140c28a076
    Author: Adriano Dal Bosco <adbosco@funtoo.org>
    Date:   Sat Apr 15 17:45:58 2023 +0000
    
        Autogenned net-irc/hexchat
    
        This is a GitHub package, but the recommended download location is the
        project webpage, where there are distribution tarballs available for
        download.  So I used the pages feature from pkgtools to get the latest
        version from a directory listing and download the sources from there.
    
        The template is based on an ebuild provided by @andifuntoo in FL-9728.

---
+++ net-kit/curated/net-irc/hexchat/autogen.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import re
+from packaging import version as pversion
+
+async def generate(hub, **pkginfo):
+
+	base_url = "https://dl.hexchat.net/hexchat/"
+
+	versions = await hub.pkgtools.pages.iter_links (
+		base_url = base_url,
+		match_fn = lambda x: re.match("hexchat-(\d+\.\d+\.\d+).tar.xz", x),
+		fixup_fn = lambda x: x.groups()[0]
+    )
+
+	versions.sort( key=lambda x: pversion.parse(x), reverse=True )
+	version = versions[0]
+	hub.pkgtools.model.log.debug(f"Versions found: {versions}")
+	hub.pkgtools.model.log.debug(f"Latest: {version}")
+
+	artifact = hub.pkgtools.ebuild.Artifact(
+		url=f'{base_url}/{pkginfo["name"]}-{version}.tar.xz'
+	)
+
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		version=version,
+		artifacts=[artifact]
+	)
+	ebuild.push()
+
+# vim: ts=4 sw=4 noet

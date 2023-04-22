77257e9c4ad1ffe7cb985677e18ae479feabc0da
seemant <seemant@funtoo.org>
2023-04-22 17:38:25 +0000

    Pull request #2895: FL-11226: update postgresql autogen to keep current with all upstream supported versions
    Merge in CORE/kit-fixups from ~SEEMANT/kit-fixups:FL-11226 to master
    
    Squashed commit of the following:
    
    commit 003a0d5b5e91a53ecfa43406e86f6a54b2d1c35a
    Author: seemant <seemant@funtoo.org>
    Date:   Fri Apr 21 06:37:39 2023 -0700
    
        minumum slot is now 11, so make all the >10 slot stuff standard
    
    commit a528f0ba0decaddd7679562d90620ae03e074aea
    Author: seemant <seemant@funtoo.org>
    Date:   Fri Apr 21 06:37:15 2023 -0700
    
        use upstream's supported versions page to autogen each of those, so that we simply keep up with upstream
    
    commit ceb7b9d2f50c3175880a752b463df979de3262e9
    Author: seemant <seemant@funtoo.org>
    Date:   Fri Apr 21 06:36:12 2023 -0700
    
        keywords will be set in autogen, to make the minium version be '*' and the rest 'next'

--- core-server-kit/curated/dev-db/postgresql/generator.py
+++ core-server-kit/curated/dev-db/postgresql/generator.py
@@ -3,29 +3,52 @@
 from bs4 import BeautifulSoup
 from packaging.version import Version
 import re
-import os.path
 
 async def generate(hub, **pkginfo):
-    download_url = "https://www.postgresql.org/ftp/source/"
     name = pkginfo.get('name')
 
-    html = await hub.pkgtools.fetch.get_page(download_url)
-    soup = BeautifulSoup(html, features="html.parser")
+    download_url = "https://ftp.postgresql.org/pub/source"
 
-    slots = [ 10, 11, 12, 13, 14 ]
-    for slot in slots:
+    # find all the supported major versions, aka slots
+    supported_versions = await find_supported_versions(hub)
+
+    for version in supported_versions:
         pkginfo_local = pkginfo.copy()
-        pkginfo_local['slot'] = slot
+        pkginfo_local['slot'] = slot = version.major
+        # Keywords are "next" for all except the minimum major version, which is "*"
+        if version > min(supported_versions):
+            keywords = "next"
+        else:
+            keywords="*"
         if slot in pkginfo['slots']:
             pkginfo_local.update(pkginfo['slots'][slot])
-        latest_version = max([a.text for a in soup.findAll("a") if re.search(f"v{slot}.([0-9.]+)", a.text)], key=lambda x: Version(x[1:]))
 
-        artifact_url = f"https://ftp.postgresql.org/pub/source/{latest_version}/{name}-{latest_version[1:]}.tar.bz2"
+        artifact_url = f"{download_url}/v{version}/{name}-{version}.tar.bz2"
         artifacts = [hub.pkgtools.ebuild.Artifact(url=artifact_url)]
 
         ebuild = hub.pkgtools.ebuild.BreezyBuild(
             **pkginfo_local,
             artifacts=artifacts,
-            version=latest_version[1:]
+            version=version,
+            keywords=keywords
         )
         ebuild.push()
+
+
+async def find_supported_versions(hub):
+    info_url = "https://www.postgresql.org/support/versioning/"
+
+    html = await hub.pkgtools.fetch.get_page(info_url)
+    table = BeautifulSoup(html, features='html.parser').findAll("tr")
+
+    # Convert the table's header rows into keys for a dict
+    keys = row_data(table.pop(0), "th")
+
+    # Convert each row of the table into a dict, using the above keys
+    data = [dict(list(zip(keys, row_data(line, "td")))) for line in table]
+
+    return [Version(item['Current minor']) for item in data if item['Supported'] == 'Yes']
+
+
+def row_data(row, data_element):
+    return [td.text.replace('Version', 'Slot') for td in row.findAll(data_element)]

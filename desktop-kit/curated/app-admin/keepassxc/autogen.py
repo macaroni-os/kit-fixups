42f99ea69ef665ae9ce54265ba395a16033fb5af
invakid404 <invakid404@riseup.net>
2023-05-17 22:44:46 +0000

    Pull request #2943: FL-11338: Use release tarball to build KeePassXC
    Merge in CORE/kit-fixups from ~INVAKID404/kit-fixups:bugfix/FL-11338-app-admin-keepassxc-2.7.5-warns-to-be-an-unstable-version to harvester/2023-05
    
    Squashed commit of the following:
    
    commit dead349f3bcb82cabd8ec7931b0ba5f71cc49f2a
    Author: invakid404 <invakid404@riseup.net>
    Date:   Thu May 18 01:05:35 2023 +0300
    
        Use release tarball to build KeePassXC
    
        The release tarball has a `.version` file which is picked up by CMake
        and sets the release type accordingly.

--- desktop-kit/curated/app-admin/keepassxc/autogen.py
+++ desktop-kit/curated/app-admin/keepassxc/autogen.py
@@ -9,7 +9,7 @@ async def generate(hub, **pkginfo):
 	github_user = "keepassxreboot"
 	github_repo = pkginfo["name"]
 
-	newpkginfo = await hub.pkgtools.github.release_gen(hub, github_user, github_repo)
+	newpkginfo = await hub.pkgtools.github.release_gen(hub, github_user, github_repo, tarball="keepassxc-{version}-src.tar.xz")
 	version = Version(newpkginfo["version"])
 
 	if version.major >= 2 and version.minor >= 7 and version.micro >= 5:

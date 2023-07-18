a08967d505174402d0c9aacbaf1f52f90a68bf1c
Daniel Robbins <drobbins@funtoo.org>
2023-07-18 16:23:12 -0400

    FL-11446: update mysql-community autogen and mask new 8.1 version for now.

--- core-server-kit/curated/dev-db/mysql-community/autogen.py
+++ core-server-kit/curated/dev-db/mysql-community/autogen.py
@@ -6,19 +6,30 @@ async def generate(hub, **pkginfo):
     homepage_url = "https://dev.mysql.com/downloads/mysql/"
     html = await hub.pkgtools.fetch.get_page(homepage_url)
     soup = BeautifulSoup(html, features="html.parser")
-    for h in soup.findAll("h1"):
-        found = re.search("MySQL Community Server ([0-9.]+)", h.text)
-        if found:
+    versions = {
+        "8.0" : {
+            "keywords" : "*",
+        },
+        "8.1" : {
+            "keywords" : ""
+        }
+    }
+    for opt in soup.findAll("option"):
+        major_minor = opt['value']
+        if major_minor in versions.keys():
+            found = re.match("([0-9.]+)", opt.text)
+            if not found:
+                continue
             version = found.groups()[0]
-            break
-    artifacts = [
-        hub.pkgtools.ebuild.Artifact(url=f"https://cdn.mysql.com/Downloads/MySQL-8.0/mysql-boost-{version}.tar.gz")
-    ]
-    ebuild = hub.pkgtools.ebuild.BreezyBuild(
-        **pkginfo,
-        artifacts=artifacts,
-        version=version,
-        revision={ "8.0.28" : "1" }
-    )
-    ebuild.push()
+            artifacts = [
+                hub.pkgtools.ebuild.Artifact(url=f"https://cdn.mysql.com/Downloads/MySQL-{major_minor}/mysql-boost-{version}.tar.gz")
+            ]
+            ebuild = hub.pkgtools.ebuild.BreezyBuild(
+                **pkginfo,
+                artifacts=artifacts,
+                version=version,
+                revision={ "8.0.28" : "1" },
+                keywords=versions[major_minor]["keywords"]
+            )
+            ebuild.push()
 

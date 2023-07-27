540971ba9bc3ebef108a1caf05cb553d4ec3499a
invakid404 <invakid404@riseup.net>
2023-07-27 20:37:25 +0000

    Pull request #2986: FL-11465: HOTFIX add mini-dashboard support to meilisearch
    Merge in CORE/kit-fixups from ~INVAKID404/kit-fixups:FL-11465-meilisearch-mini-dashboard-support to master
    
    Squashed commit of the following:
    
    commit dead18388d63d2ce20471f9c75399099ac54ecc2
    Author: invakid404 <invakid404@riseup.net>
    Date:   Thu Jul 27 01:31:28 2023 +0300
    
        Add mini-dashboard support to meilisearch

---
+++ core-server-kit/curated/app-misc/meilisearch/autogen.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import os
+import toml
+
+
+async def generate(hub, **pkginfo):
+	github_user = github_repo = "meilisearch"
+
+	pkginfo["github_user"] = github_user
+	pkginfo["github_repo"] = github_repo
+
+	release_info = await hub.pkgtools.github.release_gen(hub, github_user, github_repo)
+	pkginfo.update(release_info)
+
+	src_artifact = pkginfo["artifacts"][0]
+	pkginfo["artifacts"] = {
+		"src": src_artifact,
+	}
+
+	await src_artifact.ensure_fetched()
+	src_artifact.extract()
+
+	src_path = os.path.join(
+		src_artifact.extract_path, f"{github_user}-{github_repo}-{pkginfo['sha'][:7]}"
+	)
+
+	cargo_lock_path = os.path.join(src_path, "Cargo.lock")
+	await hub.pkgtools.rust.add_crates_bundle(
+		hub, pkginfo, cargo_lock_path=cargo_lock_path
+	)
+
+	meili_package_path = os.path.join(src_path, "meilisearch", "Cargo.toml")
+	with open(meili_package_path, "r") as meili_package_file:
+		meili_package_data = toml.load(meili_package_file)
+
+	dashboard_metadata = meili_package_data["package"]["metadata"]["mini-dashboard"]
+
+	dashboard_url = pkginfo["dashboard_url"] = dashboard_metadata["assets-url"]
+	dashboard_sha = pkginfo["dashboard_sha"] = dashboard_metadata["sha1"]
+
+	pkginfo["artifacts"]["dashboard"] = hub.pkgtools.ebuild.Artifact(
+		url=dashboard_url,
+		final_name=f"{github_repo}-mini-dashboard-{dashboard_sha}.zip",
+	)
+
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(**pkginfo)
+	ebuild.push()

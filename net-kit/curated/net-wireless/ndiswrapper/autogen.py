6b4e6fd4e15f83f844eaff69de45ea6b7e5cc207
Daniel Robbins <drobbins@funtoo.org>
2023-07-28 21:37:54 -0400

    Convert ndiswrapper to more modern get_page.

--- net-kit/curated/net-wireless/ndiswrapper/autogen.py
+++ net-kit/curated/net-wireless/ndiswrapper/autogen.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
-import json
 import re
 
-
 async def generate(hub, **pkginfo):
-	json_data = await hub.pkgtools.fetch.get_page("https://sourceforge.net/projects/ndiswrapper/best_release.json")
-	json_dict = json.loads(json_data)
+	json_dict = await hub.pkgtools.fetch.get_page("https://sourceforge.net/projects/ndiswrapper/best_release.json", is_json=True)
 	release_dict = json_dict["release"]
 	version = (re.search(f"ndiswrapper-([0-9.]*).tar.gz", release_dict["url"])).group(1)
 	url = f"http://downloads.sourceforge.net/project/ndiswrapper/stable/ndiswrapper-{version}.tar.gz"

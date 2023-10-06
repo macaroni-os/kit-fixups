431baccb81f192c2b73e3c6610afbd6abea98d6d
Daniel Robbins <drobbins@funtoo.org>
2023-10-06 15:00:09 -0400

    FL-11685: Add ability to specify src_uri: in autogen YAML for the simple generator, as a literal string (one URL) or a list (multiple.)

--- generators/simple.py
+++ generators/simple.py
@@ -4,6 +4,18 @@ GLOBAL_DEFAULTS = {}
 
 
 async def generate(hub, **pkginfo):
+	if "src_uri" in pkginfo:
+		new_src_uri = []
+		if isinstance(pkginfo["src_uri"], list):
+			artifacts = []
+			for url in pkginfo["src_uri"]:
+				new_src_uri.append(hub.Artifact(url=url))
+		elif isinstance(pkginfo["src_uri"], str):
+			new_src_uri.append(hub.Artifact(url=pkginfo["src_uri"]))
+		# Allow our default expander to be used:
+		pkginfo["artifacts"] = new_src_uri
+		del pkginfo["src_uri"]
+
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(**pkginfo)
 	ebuild.push()
 
